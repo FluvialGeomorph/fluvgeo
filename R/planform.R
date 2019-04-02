06e08c23c938b46a9d88c0d488533e1ccf696038
Michael P. Dougherty <Michael.P.Dougherty@usace.army.mil>
2019-04-02 18:38:56 -0500

    Added planform function

---
+++ R/planform.R
@@ -0,0 +1,49 @@
+#' @title Calculate planform dimensions
+#'
+#' @description Calculates planform dimensions (bend radius of curvature,
+#' meander wavelength, and meander belt width) for each bend and loop in the
+#' input `bankline_points` data frame.
+#'
+#' @export
+#' @param bankline_points  data frame; a data frame of bankline points
+#'
+#' @return Returns a data frame of bends with the calculated planform
+#' dimensions.
+#'
+#' @importFrom assertthat assert_that
+#' @importFrom dplyr lead lag
+#' @importFrom raster pointDistance
+#'
+planform <- function(bankline_points) {
+  # Check parameters
+  assert_that(check_data_structure(bankline_points, "bankline_points"),
+              msg = "'channel_features' does not meet the data specification")
+
+  # Calculate the bend radius of curvature
+  bends <- bend_radius(bankline_points)
+
+  # Calculate the meander length for each loop
+  loop_length <- meander_length(bankline_points)
+
+  # Calculate the meander width for each loop
+  loop_width <- meander_width(bankline_points)
+
+  # Join loop_length to bends
+  bends_length <- merge(x = bends, y = loop_length, by = "loop", all = TRUE)
+
+  # Join loop_width to bends1
+  bends_length_width <- merge(x = bends_length, y = loop_width, by = "loop",
+                           all = TRUE)
+
+  # Remove unneeded fields
+  fields <- c("bend_num", "loop", "bend", "bend_POINT_X", "bend_POINT_Y",
+              "POINT_X.x", "POINT_Y.x", "bend_radius", "meander_length",
+              "meander_width")
+  bends_planform <- bends_length_width[, fields]
+
+  # Rename fields
+  names(bends_planform)[names(bends_planform) == 'POINT_X.x'] <- 'loop_POINT_X'
+  names(bends_planform)[names(bends_planform) == 'POINT_Y.x'] <- 'loop_POINT_Y'
+
+  return(bends_planform)
+}
