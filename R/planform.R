c6362675863099025f58a25dd87a8d8470d8a373
Michael P. Dougherty <Michael.P.Dougherty@usace.army.mil>
2019-07-13 15:19:04 -0500

    Refactored bankline_points field names

--- R/planform.R
+++ R/planform.R
@@ -17,7 +17,7 @@
 planform <- function(bankline_points) {
   # Check parameters
   assert_that(check_data_structure(bankline_points, "bankline_points"),
-              msg = "'channel_features' does not meet the data specification")
+              msg = "'bankline_points' does not meet the data specification")
 
   # Calculate the bend radius of curvature
   bends <- bend_radius(bankline_points)
@@ -31,19 +31,19 @@ planform <- function(bankline_points) {
   # Join loop_length to bends
   bends_length <- merge(x = bends, y = loop_length, by = "loop", all = TRUE)
 
-  # Join loop_width to bends1
+  # Join loop_width to bends
   bends_length_width <- merge(x = bends_length, y = loop_width, by = "loop",
                            all = TRUE)
 
-  # Remove unneeded fields
+  # Set fields to keep
   fields <- c("bend_num", "loop", "bend", "bend_POINT_X", "bend_POINT_Y",
-              "POINT_X.x", "POINT_Y.x", "bend_radius", "meander_length",
-              "meander_width")
+              "bank_POINT_X.x", "bank_POINT_Y.x", "bend_radius",
+              "meander_length", "meander_width")
   bends_planform <- bends_length_width[, fields]
 
   # Rename fields
-  names(bends_planform)[names(bends_planform) == 'POINT_X.x'] <- 'loop_POINT_X'
-  names(bends_planform)[names(bends_planform) == 'POINT_Y.x'] <- 'loop_POINT_Y'
+  names(bends_planform)[names(bends_planform) == 'bank_POINT_X.x'] <- 'loop_POINT_X'
+  names(bends_planform)[names(bends_planform) == 'bank_POINT_Y.x'] <- 'loop_POINT_Y'
 
   return(bends_planform)
 }
