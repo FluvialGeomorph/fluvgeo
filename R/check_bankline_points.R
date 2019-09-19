425135a689bfe0cf38de7537ebf49633459cb296
Michael Dougherty <mike.p.doc.71@gmail.com>
2019-09-19 15:51:57 -0500

    Added check_bankline_points function

---
+++ R/check_bankline_points.R
@@ -0,0 +1,55 @@
+#' @title Check the validity of an `fgm` `bankline_points` data structure
+#'
+#' @description Checks that the input data structure `bankline_points` meets
+#' the requirements for this data structure.
+#'
+#' @export
+#' @param bankline_points   data frame; a `bankline_points` data structure used
+#'                          by the fgm package.
+#'
+#' @return Returns TRUE if the `bankline_points` data structure matches the
+#' requirements. The function throws an error for a data structure not matching
+#' the data specification. Returns errors describing how the the data structure
+#' doesn't match the requirement.
+#'
+#' @importFrom assertthat assert_that
+#'
+check_bankline_points <- function(bankline_points) {
+  name <- deparse(substitute(bankline_points))
+
+  assert_that(is.data.frame(bankline_points),
+              msg = paste(name, " must be a data frame"))
+  assert_that("bank" %in% colnames(bankline_points) &
+                is.character(bankline_points$bank),
+              msg = paste("Numeric field 'bank' missing from ", name))
+  assert_that("DEM_Z" %in% colnames(bankline_points) &
+                is.numeric(bankline_points$DEM_Z),
+              msg = paste("Numeric field 'DEM_Z' missing from ", name))
+  assert_that("loop" %in% colnames(bankline_points) &
+                is.numeric(bankline_points$loop),
+              msg = paste("Numeric field 'loop' missing from ", name))
+  assert_that("bend" %in% colnames(bankline_points) &
+                is.numeric(bankline_points$bend),
+              msg = paste("Numeric field 'bend' missing from ", name))
+  assert_that("position" %in% colnames(bankline_points) &
+                is.character(bankline_points$position),
+              msg = paste("Numeric field 'position' missing from ", name))
+  assert_that("bank_POINT_X" %in% colnames(bankline_points) &
+                is.numeric(bankline_points$bank_POINT_X),
+              msg = paste("Numeric field 'bank_POINT_X' missing from ", name))
+  assert_that("bank_POINT_Y" %in% colnames(bankline_points) &
+                is.numeric(bankline_points$bank_POINT_Y),
+              msg = paste("Numeric field 'bank_POINT_Y' missing from ", name))
+  assert_that("bank_POINT_M" %in% colnames(bankline_points) &
+                is.numeric(bankline_points$bank_POINT_M),
+              msg = paste("Numeric field 'bank_POINT_M' missing from ", name))
+  assert_that("valley_POINT_X" %in% colnames(bankline_points) &
+                is.numeric(bankline_points$valley_POINT_X),
+              msg = paste("Numeric field 'valley_POINT_X' missing from ", name))
+  assert_that("valley_POINT_Y" %in% colnames(bankline_points) &
+                is.numeric(bankline_points$valley_POINT_Y),
+              msg = paste("Numeric field 'valley_POINT_Y' missing from ", name))
+  assert_that("valley_POINT_M" %in% colnames(bankline_points) &
+                is.numeric(bankline_points$valley_POINT_M),
+              msg = paste("Numeric field 'valley_POINT_M' missing from ", name))
+}
--- R/check_data_structure.R
+++ R/check_data_structure.R
@@ -16,43 +16,23 @@
 #' @importFrom assertthat assert_that
 #'
 check_data_structure <-function(data_structure,
-                                data_type = c("channel_feature",
-                                              "bankline_points",
+                                data_type = c("bankline_points",
+                                              "channel_feature",
                                               "cross_section",
-                                              "slope_sinuosity",
-                                              "downhill")) {
+                                              "downhill",
+                                              "flowline",
+                                              "flowline_points",
+                                              "slope_sinuosity")) {
 
   switch(data_type,
+         bankline_points = check_bankline_points(data_structure),
          channel_feature = channel_feature_test(data_structure),
          cross_section   = cross_section_test(data_structure),
-         bankline_points = bankline_points_test(data_structure),
-         slope_sinuosity = slope_sinuosity_test(data_structure),
          downhill        = downhill_test(data_structure),
+         flowline        = flowline_test(data_structure),
+         flowline_points = flowline_points_test(data_structure),
+         slope_sinuosity = slope_sinuosity_test(data_structure),
          stop("data_structure is not an fgm data structure"))
-}
-
-downhill_test <- function(downhill_feature) {
-  # This test assumes that features (e.g. flowline, cross section sequences,
-  # banklines, centerline) are digitized from the bottom of the reach to the
-  # top of the reach (not direction of flow).
-  name <- deparse(substitute(downhill_feature))
-
-  # If features oriented from downstream to upstream: m_min_z < m_max_z
-  m_min <- min(downhill_feature$POINT_M)
-  m_max <- max(downhill_feature$POINT_M)
-  m_min_z <- downhill_feature[downhill_feature$POINT_M == m_min, ]$Z
-  m_max_z <- downhill_feature[downhill_feature$POINT_M == m_max, ]$Z
-
-  assert_that(is.data.frame(downhill_feature),
-              msg = paste(name, "must be a data frame"))
-  assert_that("POINT_M" %in% colnames(downhill_feature) &
-                is.numeric(downhill_feature$POINT_M),
-              msg = paste("Numeric field 'POINT_M' missing from", name))
-  assert_that("Z" %in% colnames(downhill_feature) &
-                is.numeric(downhill_feature$Z),
-              msg = paste("Numeric field 'Z' missing from", name))
-  assert_that(m_min_z < m_max_z,
-              msg = paste("Water doesn't flow downhill in", name))
 }
 
 channel_feature_test <- function(channel_feature) {
@@ -72,55 +52,6 @@ channel_feature_test <- function(channel_feature) {
   assert_that("POINT_M" %in% colnames(channel_feature) &
                 is.numeric(channel_feature$POINT_M),
               msg = paste("Numeric field 'POINT_M' missing from ", name))
-}
-
-slope_sinuosity_test <- function(slope_sinuosity_df) {
-  name <- deparse(substitute(slope_sinuosity_df))
-
-  assert_that(is.data.frame(slope_sinuosity_df),
-              msg = paste(name, " must be a data frame"))
-  assert_that("upstream_x" %in% colnames(slope_sinuosity_df) &
-                is.numeric(slope_sinuosity_df$upstream_x),
-              msg = paste("Numeric field 'upstream_x' missing from ", name))
-  assert_that("upstream_y" %in% colnames(slope_sinuosity_df) &
-                is.numeric(slope_sinuosity_df$upstream_y),
-              msg = paste("Numeric field 'upstream_y' missing from ", name))
-  assert_that("downstream_x" %in% colnames(slope_sinuosity_df) &
-                is.numeric(slope_sinuosity_df$downstream_x),
-              msg = paste("Numeric field 'downstream_x' missing from ", name))
-  assert_that("downstream_y" %in% colnames(slope_sinuosity_df) &
-                is.numeric(slope_sinuosity_df$downstream_y),
-              msg = paste("Numeric field 'downstream_y' missing from ", name))
-  assert_that("upstream_z" %in% colnames(slope_sinuosity_df) &
-                is.numeric(slope_sinuosity_df$upstream_z),
-              msg = paste("Numeric field 'upstream_z' missing from ", name))
-  assert_that("downstream_z" %in% colnames(slope_sinuosity_df) &
-                is.numeric(slope_sinuosity_df$downstream_z),
-              msg = paste("Numeric field 'downstream_z' missing from ", name))
-  assert_that("upstream_m" %in% colnames(slope_sinuosity_df) &
-                is.numeric(slope_sinuosity_df$upstream_m),
-              msg = paste("Numeric field 'upstream_m' missing from ", name))
-  assert_that("downstream_m" %in% colnames(slope_sinuosity_df) &
-                is.numeric(slope_sinuosity_df$downstream_m),
-              msg = paste("Numeric field 'downstream_m' missing from ", name))
-  assert_that("rise" %in% colnames(slope_sinuosity_df) &
-                is.numeric(slope_sinuosity_df$rise),
-              msg = paste("Numeric field 'rise' missing from ", name))
-  assert_that("run" %in% colnames(slope_sinuosity_df) &
-                is.numeric(slope_sinuosity_df$run),
-              msg = paste("Numeric field 'run' missing from ", name))
-  assert_that("stream_length" %in% colnames(slope_sinuosity_df) &
-                is.numeric(slope_sinuosity_df$stream_length),
-              msg = paste("Numeric field 'stream_length' missing from ", name))
-  assert_that("valley_length" %in% colnames(slope_sinuosity_df) &
-                is.numeric(slope_sinuosity_df$valley_length),
-              msg = paste("Numeric field 'valley_length' missing from ", name))
-  assert_that("sinuosity" %in% colnames(slope_sinuosity_df) &
-                is.numeric(slope_sinuosity_df$sinuosity),
-              msg = paste("Numeric field 'sinuosity' missing from ", name))
-  assert_that("slope" %in% colnames(slope_sinuosity_df) &
-                is.numeric(slope_sinuosity_df$slope),
-              msg = paste("Numeric field 'slope' missing from ", name))
 }
 
 cross_section_test <- function(cross_section) {
@@ -172,43 +103,120 @@ cross_section_test <- function(cross_section) {
               msg = paste("Numeric field 'floodprone_elev' missing from ", name))
 }
 
-bankline_points_test <- function(bankline_points) {
-  name <- deparse(substitute(bankline_points))
+downhill_test <- function(downhill_feature) {
+  # This test assumes that features (e.g. flowline, cross section sequences,
+  # banklines, centerline) are digitized from the bottom of the reach to the
+  # top of the reach (not in the direction of flow).
+  name <- deparse(substitute(downhill_feature))
 
-  assert_that(is.data.frame(bankline_points),
-              msg = paste(name, " must be a data frame"))
-  assert_that("bank" %in% colnames(bankline_points) &
-                is.character(bankline_points$bank),
-              msg = paste("Numeric field 'bank' missing from ", name))
-  assert_that("DEM_Z" %in% colnames(bankline_points) &
-                is.numeric(bankline_points$DEM_Z),
-              msg = paste("Numeric field 'DEM_Z' missing from ", name))
-  assert_that("loop" %in% colnames(bankline_points) &
-                is.numeric(bankline_points$loop),
-              msg = paste("Numeric field 'loop' missing from ", name))
-  assert_that("bend" %in% colnames(bankline_points) &
-                is.numeric(bankline_points$bend),
-              msg = paste("Numeric field 'bend' missing from ", name))
-  assert_that("position" %in% colnames(bankline_points) &
-                is.character(bankline_points$position),
-              msg = paste("Numeric field 'position' missing from ", name))
-  assert_that("bank_POINT_X" %in% colnames(bankline_points) &
-                is.numeric(bankline_points$bank_POINT_X),
-              msg = paste("Numeric field 'bank_POINT_X' missing from ", name))
-  assert_that("bank_POINT_Y" %in% colnames(bankline_points) &
-                is.numeric(bankline_points$bank_POINT_Y),
-              msg = paste("Numeric field 'bank_POINT_Y' missing from ", name))
-  assert_that("bank_POINT_M" %in% colnames(bankline_points) &
-                is.numeric(bankline_points$bank_POINT_M),
-              msg = paste("Numeric field 'bank_POINT_M' missing from ", name))
-  assert_that("valley_POINT_X" %in% colnames(bankline_points) &
-                is.numeric(bankline_points$valley_POINT_X),
-              msg = paste("Numeric field 'valley_POINT_X' missing from ", name))
-  assert_that("valley_POINT_Y" %in% colnames(bankline_points) &
-                is.numeric(bankline_points$valley_POINT_Y),
-              msg = paste("Numeric field 'valley_POINT_Y' missing from ", name))
-  assert_that("valley_POINT_M" %in% colnames(bankline_points) &
-                is.numeric(bankline_points$valley_POINT_M),
-              msg = paste("Numeric field 'valley_POINT_M' missing from ", name))
+  # If features oriented from downstream to upstream: m_min_z < m_max_z
+  m_min <- min(downhill_feature$POINT_M)
+  m_max <- max(downhill_feature$POINT_M)
+  m_min_z <- downhill_feature[downhill_feature$POINT_M == m_min, ]$Z
+  m_max_z <- downhill_feature[downhill_feature$POINT_M == m_max, ]$Z
+
+  assert_that(is.data.frame(downhill_feature),
+              msg = paste(name, "must be a data frame"))
+  assert_that("POINT_M" %in% colnames(downhill_feature) &
+                is.numeric(downhill_feature$POINT_M),
+              msg = paste("Numeric field 'POINT_M' missing from", name))
+  assert_that("Z" %in% colnames(downhill_feature) &
+                is.numeric(downhill_feature$Z),
+              msg = paste("Numeric field 'Z' missing from", name))
+  assert_that(m_min_z < m_max_z,
+              msg = paste("Water doesn't flow downhill in", name))
 }
+
+flowline_test <- function(flowline) {
+  name <- deparse(substitute(flowline))
+
+  assert_that(class(flowline) == "SpatialLinesDataFrame")
+  assert_that(is.data.frame(flowline@data),
+              msg = paste(name, "must be a data frame"))
+  assert_that("ReachName" %in% colnames(flowline@data) &
+                is.character(flowline@data$ReachName),
+              msg = paste("Character field 'ReachName' missing from ", name))
+  assert_that("from_measure" %in% colnames(flowline@data) &
+                is.numeric(flowline@data$from_measure),
+              msg = paste("Numeric field 'from_measure' missing from ", name))
+  assert_that("to_measure" %in% colnames(flowline@data) &
+                is.numeric(flowline@data$to_measure),
+              msg = paste("Numeric field 'to_measure' missing from ", name))
+}
+
+flowline_points_test <- function(flowline_points) {
+  name <- deparse(substitute(flowline_points))
+
+  assert_that(class(flowline_points) == "SpatialPointsDataFrame")
+  assert_that(is.data.frame(flowline_points@data),
+              msg = paste(name, "must be a data frame"))
+  assert_that("ReachName" %in% colnames(flowline_points@data) &
+                is.character(flowline_points@data$ReachName),
+              msg = paste("Character field 'ReachName' missing from ", name))
+  assert_that("POINT_X" %in% colnames(flowline_points@data) &
+                is.numeric(flowline_points@data$POINT_X),
+              msg = paste("Numeric field 'POINT_X' missing from ", name))
+  assert_that("POINT_Y" %in% colnames(flowline_points@data) &
+                is.numeric(flowline_points@data$POINT_Y),
+              msg = paste("Numeric field 'POINT_Y' missing from ", name))
+  assert_that("POINT_M" %in% colnames(flowline_points@data) &
+                is.numeric(flowline_points@data$POINT_M),
+              msg = paste("Numeric field 'POINT_M' missing from ", name))
+  assert_that("Z" %in% colnames(flowline_points@data) &
+                is.numeric(flowline_points@data$Z),
+              msg = paste("Numeric field 'Z' missing from ", name))
+}
+
+
+slope_sinuosity_test <- function(slope_sinuosity_df) {
+  name <- deparse(substitute(slope_sinuosity_df))
+
+  assert_that(is.data.frame(slope_sinuosity_df),
+              msg = paste(name, " must be a data frame"))
+  assert_that("upstream_x" %in% colnames(slope_sinuosity_df) &
+                is.numeric(slope_sinuosity_df$upstream_x),
+              msg = paste("Numeric field 'upstream_x' missing from ", name))
+  assert_that("upstream_y" %in% colnames(slope_sinuosity_df) &
+                is.numeric(slope_sinuosity_df$upstream_y),
+              msg = paste("Numeric field 'upstream_y' missing from ", name))
+  assert_that("downstream_x" %in% colnames(slope_sinuosity_df) &
+                is.numeric(slope_sinuosity_df$downstream_x),
+              msg = paste("Numeric field 'downstream_x' missing from ", name))
+  assert_that("downstream_y" %in% colnames(slope_sinuosity_df) &
+                is.numeric(slope_sinuosity_df$downstream_y),
+              msg = paste("Numeric field 'downstream_y' missing from ", name))
+  assert_that("upstream_z" %in% colnames(slope_sinuosity_df) &
+                is.numeric(slope_sinuosity_df$upstream_z),
+              msg = paste("Numeric field 'upstream_z' missing from ", name))
+  assert_that("downstream_z" %in% colnames(slope_sinuosity_df) &
+                is.numeric(slope_sinuosity_df$downstream_z),
+              msg = paste("Numeric field 'downstream_z' missing from ", name))
+  assert_that("upstream_m" %in% colnames(slope_sinuosity_df) &
+                is.numeric(slope_sinuosity_df$upstream_m),
+              msg = paste("Numeric field 'upstream_m' missing from ", name))
+  assert_that("downstream_m" %in% colnames(slope_sinuosity_df) &
+                is.numeric(slope_sinuosity_df$downstream_m),
+              msg = paste("Numeric field 'downstream_m' missing from ", name))
+  assert_that("rise" %in% colnames(slope_sinuosity_df) &
+                is.numeric(slope_sinuosity_df$rise),
+              msg = paste("Numeric field 'rise' missing from ", name))
+  assert_that("run" %in% colnames(slope_sinuosity_df) &
+                is.numeric(slope_sinuosity_df$run),
+              msg = paste("Numeric field 'run' missing from ", name))
+  assert_that("stream_length" %in% colnames(slope_sinuosity_df) &
+                is.numeric(slope_sinuosity_df$stream_length),
+              msg = paste("Numeric field 'stream_length' missing from ", name))
+  assert_that("valley_length" %in% colnames(slope_sinuosity_df) &
+                is.numeric(slope_sinuosity_df$valley_length),
+              msg = paste("Numeric field 'valley_length' missing from ", name))
+  assert_that("sinuosity" %in% colnames(slope_sinuosity_df) &
+                is.numeric(slope_sinuosity_df$sinuosity),
+              msg = paste("Numeric field 'sinuosity' missing from ", name))
+  assert_that("slope" %in% colnames(slope_sinuosity_df) &
+                is.numeric(slope_sinuosity_df$slope),
+              msg = paste("Numeric field 'slope' missing from ", name))
+}
+
+
+
 
