c8274eeb555c92a94c94557ba73e0b9ae440b196
Michael Dougherty <mike.p.doc.71@gmail.com>
2019-09-26 15:03:57 -0500

    Refactored check_cross_section function

--- R/check_cross_section.R
+++ R/check_cross_section.R
@@ -5,7 +5,15 @@
 #'
 #' @export
 #' @param cross_section   data frame; a `cross_section` data structure used
-#'                          by the fgm package.
+#'                        by the fgm package.
+#' @param step            character; last completed processing step. One of
+#'                        "assign_ids", "watershed_area", "river_position",
+#'                        "station_points", "loop_bend"
+#'
+#' @details Cross section feature classes evolve as different steps are
+#' performed on them. The `step` parameter allows a `cross section` data
+#' structure to be checked throughout its lifecycle. Each step defines a
+#' changing set of requirements for the `cross section` data structure.
 #'
 #' @return Returns TRUE if the `cross_section` data structure matches the
 #' requirements. The function throws an error for a data structure not matching
@@ -14,51 +22,110 @@
 #'
 #' @importFrom assertthat assert_that
 #'
-check_cross_section <- function(cross_section) {
+check_cross_section <- function(cross_section,
+                                step = c("assign_ids", "watershed_area",
+                                         "river_position", "station_points",
+                                         "loop_bend")) {
   name <- deparse(substitute(cross_section))
 
-  assert_that(is.data.frame(cross_section),
-              msg = paste(name, " must be a data frame"))
-  assert_that("bankfull_elevation" %in% colnames(cross_section) &
-                is.numeric(cross_section$bankfull_elevation),
-              msg = paste("Numeric field 'bankfull_elevation' missing from ", name))
-  assert_that("drainage_area" %in% colnames(cross_section) &
-                is.numeric(cross_section$drainage_area),
-              msg = paste("Numeric field 'drainage_area' missing from ", name))
-  assert_that("xs_area" %in% colnames(cross_section) &
-                is.numeric(cross_section$xs_area),
-              msg = paste("Numeric field 'xs_area' missing from ", name))
-  assert_that("xs_width" %in% colnames(cross_section) &
-                is.numeric(cross_section$xs_width),
-              msg = paste("Numeric field 'xs_width' missing from ", name))
-  assert_that("xs_depth" %in% colnames(cross_section) &
-                is.numeric(cross_section$xs_depth),
-              msg = paste("Numeric field 'xs_depth' missing from ", name))
-  assert_that("discharge" %in% colnames(cross_section) &
-                is.numeric(cross_section$discharge),
-              msg = paste("Numeric field 'discharge' missing from ", name))
-  assert_that("fp_area" %in% colnames(cross_section) &
-                is.numeric(cross_section$fp_area),
-              msg = paste("Numeric field 'fp_area' missing from ", name))
-  assert_that("fp_width" %in% colnames(cross_section) &
-                is.numeric(cross_section$fp_width),
-              msg = paste("Numeric field 'fp_width' missing from ", name))
-  assert_that("fp_depth" %in% colnames(cross_section) &
-                is.numeric(cross_section$fp_depth),
-              msg = paste("Numeric field 'fp_depth' missing from ", name))
-  assert_that("xs_width_depth_ratio" %in% colnames(cross_section) &
-                is.numeric(cross_section$xs_width_depth_ratio),
-              msg = paste("Numeric field 'xs_width_depth_ratio' missing from ", name))
-  assert_that("xs_entrenchment_ratio" %in% colnames(cross_section) &
-                is.numeric(cross_section$xs_entrenchment_ratio),
-              msg = paste("Numeric field 'xs_entrenchment_ratio' missing from ", name))
-  assert_that("watersurface_elev" %in% colnames(cross_section) &
-                is.numeric(cross_section$watersurface_elev),
-              msg = paste("Numeric field 'watersurface_elev' missing from ", name))
-  assert_that("bankfull_elev" %in% colnames(cross_section) &
-                is.numeric(cross_section$bankfull_elev),
-              msg = paste("Numeric field 'bankfull_elev' missing from ", name))
-  assert_that("floodprone_elev" %in% colnames(cross_section) &
-                is.numeric(cross_section$floodprone_elev),
-              msg = paste("Numeric field 'floodprone_elev' missing from ", name))
+  # Step: assign_ids
+  if(step %in% c("assign_ids", "watershed_area", "river_position",
+             "station_points", "loop_bend")) {
+    assert_that(class(cross_section)[1] == "SpatialLinesDataFrame",
+                msg = paste(name, "must be a SpatialLinesDataFrame"))
+    assert_that(is.data.frame(cross_section@data),
+                msg = paste(name, "must be a data frame"))
+    assert_that("ReachName" %in% colnames(cross_section@data) &
+                  is.character(cross_section@data$ReachName),
+                msg = paste("Character field 'ReachName' missing from", name))
+    assert_that("Seq" %in% colnames(cross_section@data) &
+                  is.numeric(cross_section@data$Seq),
+                msg = paste("Numeric field 'Seq' missing from", name))
+
+    # Check the ReachName is not empty
+    assert_that(nchar(unique(cross_section$ReachName[1])) > 0,
+                msg = paste("Field `ReachName` is empty in", name))
+
+    # Check for missing ids
+    assert_that(length(unique(cross_section$Seq)) ==
+                  max(cross_section$Seq),
+                msg = paste("Check for missing `Seq` values in", name))
+
+    # Check for duplicate ids
+    assert_that(length(unique(cross_section$Seq)) ==
+                  length(cross_section$Seq),
+                msg = paste("Check for duplicate `Seq` values in", name))
+
+  }
+  # Step: watershed_area
+  if(step %in% c("watershed_area", "river_position", "station_points",
+                 "loop_bend")) {
+    assert_that("Watershed_Area_SqMile" %in% colnames(cross_section@data) &
+                  is.numeric(cross_section@data$Watershed_Area_SqMile),
+                msg = paste("Numeric field 'Watershed_Area_SqMile' missing from",
+                            name))
+
+    # Check that all cross sections have watershed areas
+    assert_that((sum(is.na(cross_section@data$Watershed_Area_SqMi)) +
+                   sum(na.omit(cross_section@data$Watershed_Area_SqMile) == 0)) == 0,
+                msg = paste("Field `Watershed_Area_SqMile` contains missing values",
+                            name))
+  }
+
+  # Step: river_position
+  if(step %in% c("river_position", "station_points", "loop_bend")) {
+    assert_that("POINT_X" %in% colnames(cross_section@data) &
+                is.numeric(cross_section@data$POINT_X),
+                msg = paste("Numeric field 'POINT_X' missing from", name))
+    assert_that("POINT_Y" %in% colnames(cross_section@data) &
+                is.numeric(cross_section@data$POINT_Y),
+                msg = paste("Numeric field 'POINT_Y' missing from", name))
+    assert_that("POINT_M" %in% colnames(cross_section@data) &
+                is.numeric(cross_section@data$POINT_M),
+                msg = paste("Numeric field 'POINT_M' missing from", name))
+    assert_that("Z" %in% colnames(cross_section@data) &
+                is.numeric(cross_section@data$Z),
+                msg = paste("Numeric field 'Z' missing from", name))
+    assert_that("km_to_mouth" %in% colnames(cross_section@data) &
+                is.numeric(cross_section@data$km_to_mouth),
+                msg = paste("Numeric field 'km_to_mouth' missing from", name))
+
+    # Check that cross sections are numbered beginning at the downstream end
+    min_seq <- min(cross_section@data$Seq)
+    max_seq <- max(cross_section@data$Seq)
+
+    xs_1 <- cross_section@data[cross_section@data$Seq == min_seq, ]
+    min_seq_elevation <- min(xs_1$Z)
+
+    xs_n <- cross_section@data[cross_section@data$Seq == max_seq, ]
+    max_seq_elevation <- min(xs_n$Z)
+
+    assert_that(min_seq_elevation < max_seq_elevation,
+                msg = paste("Cross section numbering doesn't begin at the ",
+                            "downstream end of the reach in ", name))
+  }
+
+  # Step: station_points
+  if(step %in% c("station_points", "loop_bend")) {
+    assert_that("from_measure" %in% colnames(cross_section@data) &
+                is.numeric(cross_section@data$from_measure),
+                msg = paste("Numeric field 'from_measure' missing from", name))
+    assert_that("to_measure" %in% colnames(cross_section@data) &
+                is.numeric(cross_section@data$to_measure),
+                msg = paste("Numeric field 'to_measure' missing from", name))
+  }
+
+  # Step: loop_bend
+  if(step %in% c("loop_bend")) {
+  assert_that("loop" %in% colnames(cross_section@data) &
+                is.numeric(cross_section@data$loop),
+              msg = paste("Numeric field 'loop' missing from", name))
+  assert_that("bend" %in% colnames(cross_section@data) &
+                is.numeric(cross_section@data$bend),
+              msg = paste("Numeric field 'bend' missing from", name))
+  }
+
+  # Return TRUE if all assertions are met
+  TRUE
 }
+
---
+++ tests/testthat/test_check_cross_section.R
@@ -0,0 +1,63 @@
+library(fgm)
+context("check_cross_section")
+
+# Create testing data
+## fields at the `assign_ids` stage
+sin_rc_1 <- fgm::sin_riffle_channel_sp[, c("OBJECTID", "ReachName", "Seq")]
+## fields at the watershed_area` step
+sin_rc_2 <- fgm::sin_riffle_channel_sp[, c("OBJECTID", "ReachName", "Seq",
+                                             "Watershed_Area_SqMile")]
+## fields at the `river_position` step
+sin_rc_3 <- fgm::sin_riffle_channel_sp[, c("OBJECTID", "ReachName", "Seq",
+                                             "Watershed_Area_SqMile",
+                                             "POINT_X", "POINT_Y", "POINT_M",
+                                             "Z", "km_to_mouth")]
+## fields at the `station_points` step
+sin_rc_4 <- fgm::sin_riffle_channel_sp[, c("OBJECTID", "ReachName", "Seq",
+                                             "Watershed_Area_SqMile",
+                                             "POINT_X", "POINT_Y", "POINT_M",
+                                             "Z", "km_to_mouth",
+                                             "from_measure", "to_measure")]
+## fields at the `loop_bend` step
+sin_rc_5 <- fgm::sin_riffle_channel_sp[, c("OBJECTID", "ReachName", "Seq",
+                                           "Watershed_Area_SqMile",
+                                           "POINT_X", "POINT_Y", "POINT_M",
+                                           "Z", "km_to_mouth",
+                                           "from_measure", "to_measure",
+                                           "loop", "bend")]
+
+
+test_that("check step `assign_ids`", {
+  expect_true(check_cross_section(sin_rc_1, "assign_ids"))
+  expect_true(check_cross_section(fgm::sin_riffle_channel_sp, "assign_ids"))
+  expect_true(check_cross_section(fgm::sin_riffle_floodplain_sp, "assign_ids"))
+  expect_error(check_cross_section(fgm::sin_features_sp, "assign_ids"))
+})
+
+test_that("check step `watershed_area`", {
+  expect_true(check_cross_section(sin_rc_2, "watershed_area"))
+  expect_true(check_cross_section(fgm::sin_riffle_channel_sp, "watershed_area"))
+  expect_true(check_cross_section(fgm::sin_riffle_floodplain_sp, "watershed_area"))
+  expect_error(check_cross_section(fgm::sin_features_sp, "watershed_area"))
+})
+
+test_that("check step `river_position`", {  # Step: river_position
+  expect_true(check_cross_section(sin_rc_3, "river_position"))
+  expect_true(check_cross_section(fgm::sin_riffle_channel_sp, "river_position"))
+  expect_true(check_cross_section(fgm::sin_riffle_floodplain_sp, "river_position"))
+  expect_error(check_cross_section(fgm::sin_features_sp, "river_position"))
+})
+
+test_that("check step `station_points`", {
+  expect_true(check_cross_section(sin_rc_4, "station_points"))
+  expect_true(check_cross_section(fgm::sin_riffle_channel_sp, "station_points"))
+  expect_true(check_cross_section(fgm::sin_riffle_floodplain_sp, "station_points"))
+  expect_error(check_cross_section(fgm::sin_features_sp, "station_points"))
+})
+
+test_that("check step `loop_bend`", {
+  expect_true(check_cross_section(sin_rc_5, "loop_bend"))
+  expect_true(check_cross_section(fgm::sin_riffle_channel_sp, "loop_bend"))
+  expect_true(check_cross_section(fgm::sin_riffle_floodplain_sp, "loop_bend"))
+  expect_error(check_cross_section(fgm::sin_features_sp, "loop_bend"))
+})
