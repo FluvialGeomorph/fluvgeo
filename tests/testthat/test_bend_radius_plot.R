d3f83812f5fe44ba9fdf0aab8c5719b77fd85728
Michael Dougherty <mike.p.doc.71@gmail.com>
2019-09-28 14:21:39 -0500

    Updated bend_radius_plot test

--- tests/testthat/test_bend_radius_plot.R
+++ tests/testthat/test_bend_radius_plot.R
@@ -1,12 +1,15 @@
 library(fgm)
 context("bend_radius_plot")
 
-br_plot <- bend_raduis_plot(fgm::sin_bankline_points_sp,
-                            loop = 1,
-                            bend = 1,
-                            coord_system = "UTM 16N")
+br_plot_1 <- bend_raduis_plot(fgm::sin_bankline_points_sp, 1, 1, "UTM 16N")
+br_plot_2 <- bend_raduis_plot(fgm::sin_bankline_points_sp, 2, 1, "UTM 16N")
 
-test_that("check bend_radius data structure", {
-  expect_true("ggplot" %in% class(br_plot))
-  expect_error(print(br_plot), NA)
+test_that("check bend_radius_plot 1", {
+  expect_true("ggplot" %in% class(br_plot_1))
+  expect_error(print(br_plot_1), NA)
+})
+
+test_that("check bend_radius_plot 2", {
+  expect_true("ggplot" %in% class(br_plot_2))
+  expect_error(print(br_plot_2), NA)
 })
