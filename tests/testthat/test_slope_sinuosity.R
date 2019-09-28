f0eb8c6c389f994b93f205f6d4b42d80877641fb
Michael Dougherty <mike.p.doc.71@gmail.com>
2019-09-28 18:04:12 -0500

    Updated tests for slope_sinuosity

--- tests/testthat/test_slope_sinuosity.R
+++ tests/testthat/test_slope_sinuosity.R
@@ -1,29 +1,16 @@
 library(fgm)
 context("slope_sinuosity")
 
-# Extract attribute data from the fgm::sin_flowline_points SpatialPointsDataFrame
-sin_flowline_points_df <- fgm::sin_flowline_points@data
-
-# Extract data from the fgm::sin_riffle SpatialLinesDataFrame
-sin_riffle_df <- fgm::sin_riffle@data
-
 # Call the slope_sinuosity function for a flowline
-sin_flowline_ss <- slope_sinuosity(sin_flowline_points_df,
+sin_flowline_ss <- slope_sinuosity(fgm::sin_flowline_points_sp,
                                    lead_n = 1000, lag_n = 0)
 
 # Call the slope_sinuosity function for a cross section
-sin_riffle_ss <- slope_sinuosity(sin_riffle_df,
-                                 lead_n = 1, lag_n = 0,
-                                 loess_span = 5)
+sin_riffle_channel_ss <- slope_sinuosity(fgm::sin_riffle_channel_sp,
+                                         lead_n = 1, lag_n = 0,
+                                         loess_span = 5)
 
 test_that("Check output data structures", {
-  assert_that(check_data_structure(sin_flowline_ss, "slope_sinuosity"),
-              msg = "'sin_flowline_ss' does not meet the data specification")
-  assert_that(check_data_structure(sin_riffle_ss, "slope_sinuosity"),
-              msg = "'sin_riffle_ss' does not meet the data specification")
-})
-
-test_that("Check that water flows downhill", {
-  assert_that(check_data_structure(sin_flowline_ss, "downhill"),
-              msg = "'sin_flowline_ss' does not flow downhill")
+  expect_true(check_data_structure(sin_flowline_ss, "slope_sinuosity"))
+  expect_true(check_data_structure(sin_riffle_channel_ss, "slope_sinuosity"))
 })
