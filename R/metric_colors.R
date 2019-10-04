7e9f9b463172ff60fb6d80586cd660db6715cbf6
Michael Dougherty <mike.p.doc.71@gmail.com>
2019-10-04 16:14:07 -0500

    Added examples for metric_colors

--- R/metric_colors.R
+++ R/metric_colors.R
@@ -9,6 +9,20 @@
 #'
 #' @return a vector of colors
 #'
+#' @examples
+#' # Define metric objects
+#'wdr3 <- new(Class = "FluvialGeomorphicMetric",
+#'            metric = "Width Depth Ratio",
+#'            definition = "bankfull width / bankfull depth",
+#'            variable = "xs_width_depth_ratio",
+#'            threshold_breaks = c(0, 10, 20, Inf),
+#'            threshold_labels = c("Incised",
+#'                                 "Stable",
+#'                                 "Overwidened"),
+#'            source = "Dunn & Leopold, 1978")
+#'
+#' # Get a set of colors for a `FluvialGeomorphicMetric` object
+#' cols <- metric_colors(wdr3)
 #'
 metric_colors <- function(metric) {
   #determine the number of colors needed
