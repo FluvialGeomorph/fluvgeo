9fc0b0968bda61a8769a854dd9bc45c02291f24a
Michael Dougherty <mike.p.doc.71@gmail.com>
2019-10-25 16:06:49 -0500

    Updated check_valleyline test

--- tests/testthat/test_check_valleyline.R
+++ tests/testthat/test_check_valleyline.R
@@ -5,8 +5,8 @@ test_that("check valleyline", {
   expect_true(check_valleyline(fgm::sin_valleyline_sp))
 })
 
-test_that("not valleyline points", {
-  expect_error(check_valleyline(fgm::sin_flowline_sp))
+test_that("check not valleyline", {
+  expect_error(check_valleyline(fgm::sin_features_sp))
   expect_error(check_valleyline(fgm::sin_loop_points_sp))
   expect_error(check_valleyline(fgm::sin_bankline_points_sp))
 })
