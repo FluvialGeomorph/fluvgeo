768a20a02984c277bf0b5a8c3b515c82b4c90e39
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-06-13 09:57:13 -0500

    Created new fc2sf function

---
+++ R/fc2sf.R
@@ -0,0 +1,15 @@
+#' @title Convert ESRI .gdb feature class to `sf`
+#'
+#' @description Converts an ESRI geodatabase (.gdb) feature class to the `sf`
+#' spatial object format.
+#'
+#' @param fc_path  character; Path to the feature class (e.g.,
+#'                 "C:/parent_folder/geodatabase.gdb/feature_class").
+#'
+#' @return An `sf` object.
+#'
+fc2sf <- function(fc_path) {
+  gdb <- dirname(fc_path)
+  fc <- basename(fc_path)
+  fc_sf <- sf::st_read(gdb, fc)
+}
