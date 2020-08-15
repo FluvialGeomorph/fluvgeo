97e525c88efe2136610c3a73e472f2b658a6f739
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-08-15 18:05:17 -0500

    updated check_banklines to ensure it handles sf objects

--- R/check_banklines.R
+++ R/check_banklines.R
@@ -17,38 +17,46 @@
 check_banklines <- function(banklines) {
   name <- deparse(substitute(banklines))
 
-  assert_that(class(banklines)[1] == "SpatialLinesDataFrame",
-              msg = paste(name, " must be a SpatialLinesDataFrame"))
-  assert_that(is.data.frame(banklines@data),
+  if(class(banklines)[1] == "SpatialLinesDataFrame") {
+    banklines_df <- banklines@data
+  }
+  if(class(banklines)[1] == "sf") {
+    banklines_df <- banklines
+  }
+
+  assert_that((class(banklines)[1] == "SpatialLinesDataFrame") |
+              (class(banklines)[1] == "sf")  ,
+              msg = paste(name, " must be a SpatialLinesDataFrame or sf"))
+  assert_that(is.data.frame(banklines_df),
               msg = paste(name, " must be a data frame"))
-  assert_that("ReachName" %in% colnames(banklines@data) &
-                is.character(banklines@data$ReachName),
+  assert_that("ReachName" %in% colnames(banklines_df) &
+                is.character(banklines_df$ReachName),
               msg = paste("Character field 'ReachName' missing from", name))
-  assert_that("bank" %in% colnames(banklines@data) &
-                is.character(banklines@data$bank),
+  assert_that("bank" %in% colnames(banklines_df) &
+                is.character(banklines_df$bank),
               msg = paste("Numeric field 'bank' missing from ", name))
-  assert_that("from_measure" %in% colnames(banklines@data) &
-                is.numeric(banklines@data$from_measure),
+  assert_that("from_measure" %in% colnames(banklines_df) &
+                is.numeric(banklines_df$from_measure),
               msg = paste("Numeric field 'from_measure' missing from", name))
-  assert_that("to_measure" %in% colnames(banklines@data) &
-                is.numeric(banklines@data$to_measure),
+  assert_that("to_measure" %in% colnames(banklines_df) &
+                is.numeric(banklines_df$to_measure),
               msg = paste("Numeric field 'to_measure' missing from", name))
 
   # Check that there are two and only two records
-  assert_that(length(unique(banklines@data$OBJECTID)) == 2,
+  assert_that(length(unique(banklines_df$bank_id)) == 2,
               msg = paste(name, "must have two and only two records, one for",
                           "the left descending bank and another for the right."))
 
   # Check that the `bank` field is populated
-  assert_that(length(banklines@data[banklines@data$bank ==
+  assert_that(length(banklines_df[banklines_df$bank ==
                                       "right descending", ]$bank) == 1,
               msg = paste("The `right descending` bank is missing from", name))
-  assert_that(length(banklines@data[banklines@data$bank ==
+  assert_that(length(banklines_df[banklines_df$bank ==
                                       "left descending", ]$bank) == 1,
               msg = paste("The `left descending` bank is missing from", name))
 
   # Check that each bankline has greater than zero length
-  assert_that(all(banklines@data$from_measure < banklines@data$to_measure),
+  assert_that(all(banklines_df$from_measure < banklines_df$to_measure),
               msg = paste("Banklines", name,
                           "appears to have banks of zero length"))
 
--- tests/testthat/test_check_banklines.R
+++ tests/testthat/test_check_banklines.R
@@ -1,6 +1,16 @@
 library(fluvgeo)
 context("check_banklines")
 
+# sf
+# Get feature class test data in sf format
+banklines_fc <- file.path(system.file("extdata", "testing_data.gdb",
+                                      package = "fluvgeo"),
+                          "banklines")
+# Convert feature classes to an sf objects
+banklines_sf <- fluvgeo::fc2sf(banklines_fc)
+
+
+# sp
 test_that("check banklines", {
   expect_true(check_banklines(fluvgeo::sin_banklines_sp))
 })
@@ -9,4 +19,9 @@ test_that("not bankline points", {
   expect_error(check_banklines(fluvgeo::sin_flowline_sp))
   expect_error(check_banklines(fluvgeo::sin_loop_points_sp))
   expect_error(check_banklines(fluvgeo::sin_bankline_points_sp))
+})
+
+# sf
+test_that("check banklines", {
+  expect_true(check_banklines(banklines_sf))
 })
