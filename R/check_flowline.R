b334bd65cfd47d3cc422fac89e3efaf2e2e1e913
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-08-15 16:46:29 -0500

    updated check_flowline to ensure it handle sf objects

--- R/check_flowline.R
+++ R/check_flowline.R
@@ -21,36 +21,44 @@ check_flowline <- function(flowline,
 
   name <- deparse(substitute(flowline))
 
+  if(class(flowline)[1] == "SpatialLinesDataFrame") {
+    flowline_df <- flowline@data
+  }
+  if(class(flowline)[1] == "sf") {
+    flowline_df <- flowline
+  }
+
   # Step: create_flowline
   if(step %in% c("create_flowline", "profile_points")) {
-  assert_that(class(flowline)[1] == "SpatialLinesDataFrame",
-              msg = paste(name, "must be a SpatialLinesDataFrame"))
-  assert_that(is.data.frame(flowline@data),
+  assert_that((class(flowline)[1] == "SpatialLinesDataFrame") |
+              (class(flowline)[1] == "sf"),
+              msg = paste(name, "must be a SpatialLinesDataFrame or sf object"))
+  assert_that(is.data.frame(flowline_df),
               msg = paste(name, "must be a data frame"))
-  assert_that("ReachName" %in% colnames(flowline@data) &
-                is.character(flowline@data$ReachName),
+  assert_that("ReachName" %in% colnames(flowline_df) &
+                is.character(flowline_df$ReachName),
               msg = paste("Character field 'ReachName' missing from", name))
 
   # Check the field `ReachName` is not empty
-  assert_that(nchar(unique(flowline@data$ReachName[1])) > 0,
+  assert_that(nchar(unique(flowline_df$ReachName[1])) > 0,
               msg = paste("Field `ReachName` is empty in", name))
 
   # Check that there is only one flowline
-  assert_that(length(flowline@data$ReachName) == 1,
+  assert_that(length(flowline_df$ReachName) == 1,
               msg = paste("Flowline", name, "can only have one record"))
   }
 
   # Step: profile_points
   if(step %in% c("profile_points")) {
-  assert_that("from_measure" %in% colnames(flowline@data) &
-                is.numeric(flowline@data$from_measure),
+  assert_that("from_measure" %in% colnames(flowline_df) &
+                is.numeric(flowline_df$from_measure),
               msg = paste("Numeric field 'from_measure' missing from", name))
-  assert_that("to_measure" %in% colnames(flowline@data) &
-                is.numeric(flowline@data$to_measure),
+  assert_that("to_measure" %in% colnames(flowline_df) &
+                is.numeric(flowline_df$to_measure),
               msg = paste("Numeric field 'to_measure' missing from", name))
 
   # Check that flowline has greater than zero length
-  assert_that(flowline@data$from_measure < flowline@data$to_measure,
+  assert_that(flowline_df$from_measure < flowline_df$to_measure,
               msg = paste("The flowline", name, "appears to have zero length"))
   }
 
