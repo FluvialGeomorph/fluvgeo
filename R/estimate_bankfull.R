b00d4f9064e6271e626c800bd84845dcbf54728f
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-09-04 14:34:01 -0500

    added longitudinal profile and cross section metrics to estimate_bankfull_report

--- R/estimate_bankfull.R
+++ R/estimate_bankfull.R
@@ -1,11 +1,16 @@
 #' @title Estimate bankfull report
 #'
-#' @description Produces the bankfull estimate report.
+#' @description Produces the Estimate Bankfull report.
 #'
 #' @export
-#' @param xs_points           data frame; a data frame of cross section points
-#' @param streams             character vector; The stream names in the study
-#'                            area.
+#' @param stream              character; The stream name. The stream name must
+#'                            match a stream name in `ReachName` field in the
+#'                            other parameters.
+#' @param xs_dims_fc          character; The path to the "xs_dims" feature
+#'                            class.
+#' @param xs_points_fc        character; The path to a `xs_points` feature
+#'                            class for the first time period.
+#' @param features_fc         character; The path to a `features` feature class.
 #' @param regions             character; The regions that a dimension will be
 #'                            calculated for. See the regional_curves$region
 #'                            field for a complete list.
@@ -17,65 +22,60 @@
 #'                            geometry.
 #' @param stat                character; The statistic to graph "RMSE", "MAE"
 #'                            (the default).
+#' @param label_xs            logical; Label cross sections?
+#' @param profile_units       character; the units of the longitudinal profile.
+#'                            One of "kilometers", "meters", "miles", or "feet"
 #' @param output_dir          character; The output directory.
 #' @param output_format       character; The output format of the report. One
 #'                            of "html_document", "word_document",
 #'                            "pdf_document".
 #'
-#' @return A report written to the file system in the output fromat requested.
+#' @return Produces a FluvialGeomorph Estimate Bankfull report in the
+#' `output_dir` in the requested file format.
 #'
 #' @examples
-#' # Extract attribute data from the fluvgeo::sin_xs_points SpatialPointsDataFrame
-#' sin_xs_points_df <- fluvgeo::sin_riffle_floodplain_points_sp@@data
 #'
-#' # Set variable values
-#' streams <- c("Sinsinawa")
-#' regions <- c("Eastern United States", "IN Central Till Plain")
-#' bankfull_elevations <- seq(103, 104, 0.1)
-#' bf_estimate <- 103.5
-#' stat <- "MAE"
-#' output_dir <- Sys.getenv("HOME")
-#' output_format <- "html_document"
 #'
-#' # Call the estimate_bankfull function with test data
-#' estimate_bankfull(xs_points = sin_xs_points_df,
-#'                   streams = streams,
-#'                   regions = regions,
-#'                   bankfull_elevations = bankfull_elevations,
-#'                   bf_estimate = bf_estimate,
-#'                   stat = stat,
-#'                   output_dir = output_dir,
-#'                   output_format = output_format)
-#'
-estimate_bankfull <- function(xs_points, streams, regions, bankfull_elevations,
-                              bf_estimate, stat,
+estimate_bankfull <- function(stream, xs_dims_fc, xs_points_fc, features_fc,
+                              regions, bankfull_elevations, bf_estimate,
+                              stat, label_xs, profile_units,
                               output_dir, output_format) {
-  # Iterate through streams
-  for (g in streams) {
-    # Construct output_file path
-    if (output_format == "html_document") {extension <- ".html"}
-    if (output_format == "word_document") {extension <- ".docx"}
-    if (output_format == "pdf_document")  {extension <- ".pdf"}
-    output_file <- file.path(output_dir, paste0("bankfull_estimate_",
-                                                g, "_", bf_estimate, extension))
 
-    # Subset xs_points for the current stream
-    xs_pts <- xs_points[xs_points$ReachName == g, ]
+  # Convert feature classes to sf objects
+  xs_dims_sf   <- fluvgeo::fc2sf(xs_dims_fc)
+  xs_points_sf <- fluvgeo::fc2sf(xs_points_fc)
+  features_sf  <- fluvgeo::fc2sf(features_fc)
 
-    # Render the report for the current stream
-    rmarkdown::render(input = system.file("reports",
-                                          "estimate_bankfull_report.Rmd",
-                                          package = "fluvgeo"),
-                      output_format = output_format,
-                      output_options = list(self_contained = TRUE),
-                      params = list(xs_points = xs_pts,
-                                    stream = g,
-                                    regions = regions,
-                                    bankfull_elevations = bankfull_elevations,
-                                    bf_estimate = bf_estimate,
-                                    stat = stat,
-                                    output_format = output_format),
-                      output_file = output_file,
-                      quiet = FALSE)
-  }
+  # Set report parameters
+  report_params <- list("stream" = stream,
+                        "xs_dims_sf" = xs_dims_sf,
+                        "xs_points_sf" = xs_points_sf,
+                        "features_sf" = features_sf,
+                        "regions" = regions,
+                        "bankfull_elevations" = bankfull_elevations,
+                        "bf_estimate" = bf_estimate,
+                        "stat" = stat,
+                        "label_xs" = label_xs,
+                        "profile_units" = profile_units,
+                        "output_format" = output_format)
+
+  # Define the report to use
+  report_template <- system.file("reports", "estimate_bankfull_report.Rmd",
+                                 package = "fluvgeo")
+
+  # Construct output_file path
+  if (output_format == "html_document") {extension <- ".html"}
+  if (output_format == "word_document") {extension <- ".docx"}
+  if (output_format == "pdf_document")  {extension <- ".pdf"}
+  bf_est <- gsub(".", "_", bf_estimate, fixed = TRUE)
+  output_file <- file.path(output_dir, paste0("bankfull_estimate_", stream,
+                                              "_", bf_est, extension))
+
+  # Render the report
+  rmarkdown::render(input = report_template,
+                    output_format = output_format,
+                    output_options = list(self_contained = TRUE),
+                    params = report_params,
+                    output_file = output_file,
+                    quiet = FALSE)
 }
--- tests/testthat/test_estimate_bankfull.R
+++ tests/testthat/test_estimate_bankfull.R
@@ -1,34 +1,63 @@
 library(fluvgeo)
 context("estimate_bankfull")
 
-# Extract attribute data from the fluvgeo::sin_xs_points SpatialPointsDataFrame
-sin_xs_points_df <- fluvgeo::sin_riffle_floodplain_points_sp@data
+# Helper functions
+skip_if_no_arc <- function() {
+  testthat::skip_if_not_installed("arcgisbinding")
+}
 
-# Set variable values
-streams <- c("Sinsinawa")
-regions <- c("Eastern United States", "IN Central Till Plain")
+load_libraries <- function() {
+  library(sp)
+  library(sf)
+  library(tmap)
+  library(arcgisbinding)
+  arcgisbinding::arc.check_product()
+}
+
+# sf
+stream <- "Sinsinawa"
+
+xs_dims_fc    <- file.path(system.file("extdata", "testing_data.gdb",
+                                       package = "fluvgeo"),
+                           "riffle_channel_dims")
+xs_points_fc  <- file.path(system.file("extdata", "testing_data.gdb",
+                                       package = "fluvgeo"),
+                           "riffle_channel_points")
+features_fc   <- file.path(system.file("extdata", "testing_data.gdb",
+                                       package = "fluvgeo"),
+                           "features")
+
+regions <- c("Eastern United States", "Illinois River")
 bankfull_elevations <- seq(103, 104, 0.1)
 bf_estimate <- 103.5
 stat <- "MAE"
+label_xs <- TRUE
+profile_units <- "miles"
 output_dir <- Sys.getenv("HOME")
-output_format <- "html_document"
 
-# Call the estimate_bankfull function with test data
-estimate_bankfull(xs_points = sin_xs_points_df,
-                  streams = streams,
-                  regions = regions,
-                  bankfull_elevations = bankfull_elevations,
-                  bf_estimate = bf_estimate,
-                  stat = stat,
-                  output_dir = output_dir,
-                  output_format = output_format)
 
-# Can be tested on all systems
 test_that("The output html report exists", {
+  skip_if_no_arc()
+  load_libraries()
+
+  output_format <- "html_document"
   output_file <- file.path(output_dir, paste0("bankfull_estimate_",
-                                              "Sinsinawa_103.5.html"))
+                                              "Sinsinawa_103_5.html"))
+
+  # Call the estimate_bankfull function with test data
+  estimate_bankfull(stream = stream,
+                    xs_dims_fc = xs_dims_fc,
+                    xs_points_fc = xs_points_fc,
+                    features_fc = features_fc,
+                    regions = regions,
+                    bankfull_elevations = bankfull_elevations,
+                    bf_estimate = bf_estimate,
+                    stat = stat,
+                    label_xs = label_xs,
+                    profile_units = profile_units,
+                    output_dir = output_dir,
+                    output_format = output_format)
   expect_true(file.exists(output_file))
-  #file.remove(output_file)
 })
 
 # Only test on systems with a working Tex distribution
