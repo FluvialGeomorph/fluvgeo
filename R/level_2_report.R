015626fccd8a23c2c33e93391d1323678955d449
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-09-07 12:36:53 -0500

    updated Level 2 report

--- R/level_2_report.R
+++ R/level_2_report.R
@@ -9,9 +9,20 @@
 #' @param flowline_fc        character; The path to a `flowline` feature class.
 #' @param xs_fc              character; The path to the cross section feature
 #'                           class.
-#' @param xs_points_fc       character; The path to a `xs_points` feature
-#'                           class for the first time period.
 #' @param xs_dims_fc         character; The path to the "xs_dims" feature class.
+#' @param xs_points_1        character; The path to a `xs_points` feature
+#'                           class for the "base year".
+#' @param xs_points_2        character; The path to a `xs_points` feature
+#'                           class for the second time period.
+#' @param xs_points_3        character; The path to a `xs_points` feature
+#'                           class for the third time period.
+#' @param xs_points_4        character; The path to a `xs_points` feature
+#'                           class for the fourth time period.
+#' @param survey_name_1      character: The name or date of the "base year"
+#'                           survey.
+#' @param survey_name_2      character: The name or date of the second survey.
+#' @param survey_name_3      character: The name or date of the third survey.
+#' @param survey_name_4      character: The name or date of the fourth survey.
 #' @param dem                character; The path to the DEM raster.
 #' @param banklines_fc       character: the path to the banklines feature class.
 #' @param features_fc        character; The path to a `features` feature class.
@@ -20,12 +31,20 @@
 #' @param regions            character vector; Regions to calculate hydraulic
 #'                           dimensions for. See the `RegionalCurve` package for
 #'                           a list of regions.
-#' @param extent_factor      numeric; The extent factor used to control the
-#'                           extent of cross section site maps.
 #' @param label_xs           logical; Label cross sections?
 #' @param show_xs_map        logical; Add the cross section maps to the report?
 #' @param profile_units      character; the units of the longitudinal profile.
-#'                           One of "kilometers", "meters", "miles", or "feet"
+#'                           One of "kilometers", "meters", "miles", or "feet".
+#' @param aerial             logical; Display an overview map with an aerial
+#'                           photo background?
+#' @param elevation          logical; Display an overview map with an elevation
+#'                           background?
+#' @param xs_label_freq      numeric; An integer indicating the frequency of
+#'                           cross section labels.
+#' @param exaggeration       numeric; The degree of terrain exaggeration.
+#' @param extent_factor      numeric; The amount the extent is expanded around
+#'                           the cross section feature class. Values greater
+#'                           than one zoom out, values less than one zoom in.
 #' @param output_dir         character; The path to the folder in which to
 #'                           write the report.
 #' @param output_format      character; The file format of the report. One of
@@ -36,16 +55,35 @@
 #'
 #' @importFrom rmarkdown render
 #'
-level_2_report <- function(stream, flowline_fc, xs_fc, xs_points_fc,
-                           xs_dims_fc, dem, banklines_fc, features_fc,
-                           bf_estimate, regions, extent_factor, label_xs,
-                           show_xs_map = FALSE,
-                           profile_units, output_dir, output_format) {
+level_2_report <- function(stream, flowline_fc, xs_fc, xs_dims_fc,
+                           xs_points_1, xs_points_2,
+                           xs_points_3, xs_points_4,
+                           survey_name_1, survey_name_2,
+                           survey_name_3, survey_name_4,
+                           dem, banklines_fc, features_fc,
+                           bf_estimate, regions, label_xs,
+                           show_xs_map = FALSE, profile_units,
+                           aerial = TRUE, elevation = FALSE,
+                           xs_label_freq = 5, exaggeration = 10,
+                           extent_factor = 1.2,
+                           output_dir, output_format) {
+
+  # Create list of survey paths
+  xs_points_paths <- list(xs_points_1, xs_points_2, xs_points_3, xs_points_4)
+
+  # Name the survey paths list by their survey names
+  survey_names <- c(survey_name_1, survey_name_2, survey_name_3, survey_name_4)
+  xs_points_paths <- setNames(xs_points_paths, survey_names)
+
+  # Eliminate empty surveys
+  xs_points_paths <- purrr::discard(xs_points_paths, is.null)
+
+  # Convert list of survey paths to list of sf objects
+  xs_pts_sf_list <- purrr::map(xs_points_paths, fluvgeo::fc2sf)
 
   # Convert feature classes to sf objects
   flowline_sf  <- fluvgeo::fc2sf(flowline_fc)
   xs_sf        <- fluvgeo::fc2sf(xs_fc)
-  xs_points_sf <- fluvgeo::fc2sf(xs_points_fc)
   xs_dims_sf   <- fluvgeo::fc2sf(xs_dims_fc)
   banklines_sf <- fluvgeo::fc2sf(banklines_fc)
   features_sf  <- fluvgeo::fc2sf(features_fc)
@@ -54,17 +92,21 @@ level_2_report <- function(stream, flowline_fc, xs_fc, xs_points_fc,
   report_params <- list("stream" = stream,
                         "flowline_sf" = flowline_sf,
                         "xs_sf" = xs_sf,
-                        "xs_points_sf" = xs_points_sf,
                         "xs_dims_sf" = xs_dims_sf,
+                        "xs_pts_sf_list" = xs_pts_sf_list,
                         "dem" = dem,
                         "banklines_sf" = banklines_sf,
                         "features_sf" = features_sf,
                         "bf_estimate" = bf_estimate,
                         "regions" = regions,
-                        "extent_factor" = extent_factor,
                         "label_xs" = label_xs,
                         "show_xs_map" = show_xs_map,
                         "profile_units" = profile_units,
+                        "aerial" = aerial,
+                        "elevation" = elevation,
+                        "xs_label_freq" = xs_label_freq,
+                        "exaggeration" = exaggeration,
+                        "extent_factor" = extent_factor,
                         "output_format" = output_format)
 
   # Define the report to use
@@ -72,11 +114,12 @@ level_2_report <- function(stream, flowline_fc, xs_fc, xs_points_fc,
                                  package = "fluvgeo")
 
   # Construct output_file path
-  stream_name <- gsub(" ", "_", stream)
   if (output_format == "html_document") {extension <- ".html"}
   if (output_format == "word_document") {extension <- ".docx"}
   if (output_format == "pdf_document")  {extension <- ".pdf"}
-  output_file <- file.path(output_dir, paste0(stream_name,
+  stream_name <- gsub(" ", "_", stream, fixed = TRUE)
+  bf_est <- gsub(".", "_", bf_estimate, fixed = TRUE)
+  output_file <- file.path(output_dir, paste0(stream_name, "_", bf_est,
                                               "_level_2_report", extension))
 
   # Render the report
--- tests/testthat/test_level_2_report.R
+++ tests/testthat/test_level_2_report.R
@@ -15,51 +15,91 @@ load_libraries <- function() {
 }
 
 # sf
-stream <- "Sinsinawa"
-flowline_fc   <- file.path(system.file("extdata", "testing_data.gdb",
+stream <- "Cole Creek R1"
+flowline_fc   <- file.path(system.file("extdata", "testing_Cole_2016.gdb",
                                           package = "fluvgeo"),
                            "flowline")
-xs_fc         <- file.path(system.file("extdata", "testing_data.gdb",
+xs_fc         <- file.path(system.file("extdata", "testing_Cole_2016.gdb",
                                           package = "fluvgeo"),
-                           "riffle_floodplain")
-xs_points_fc  <- file.path(system.file("extdata", "testing_data.gdb",
-                                          package = "fluvgeo"),
-                           "riffle_floodplain_points")
-xs_dims_fc    <- file.path(system.file("extdata", "testing_data.gdb",
+                           "xs_250_25")
+xs_dims_fc    <- file.path(system.file("extdata", "testing_Cole_2016.gdb",
                                        package = "fluvgeo"),
-                           "riffle_floodplain_dims")
-dem           <- file.path(system.file("extdata", "testing_raster.gdb",
+                           "xs_250_25_dims")
+xs_points_1 <- file.path(system.file("extdata", "testing_Cole_2016.gdb",
+                                     package = "fluvgeo"),
+                         "xs_250_25_points")
+xs_points_2 <- file.path(system.file("extdata", "testing_Cole_2010.gdb",
+                                     package = "fluvgeo"),
+                         "xs_250_25_points")
+xs_points_3 <- file.path(system.file("extdata", "testing_Cole_2004.gdb",
+                                     package = "fluvgeo"),
+                         "xs_250_25_points")
+xs_points_4 <- NULL
+survey_name_1 <- "2016"
+survey_name_2 <- "2010"
+survey_name_3 <- "2004"
+survey_name_4 <- NULL
+dem           <- file.path(system.file("extdata", "testing_Cole_2016.gdb",
                                        package = "fluvgeo"),
-                           "dem_1m")
-banklines_fc  <- file.path(system.file("extdata", "testing_data.gdb",
+                           "dem_2016_ft")
+banklines_fc  <- file.path(system.file("extdata", "testing_Cole_2016.gdb",
                                        package = "fluvgeo"),
                            "banklines")
-features_fc   <- file.path(system.file("extdata", "testing_data.gdb",
+features_fc   <- file.path(system.file("extdata", "testing_Cole_2016.gdb",
                                        package = "fluvgeo"),
                            "features")
 bf_estimate   <- 103.5
-regions       <- c("Illinois River", "IL River LTE 300",
-                   "Eastern United States")
-
-extent_factor <- 1
+regions       <- c("USA", "Eastern United States")
 label_xs      <- TRUE
 show_xs_map   <- FALSE
 profile_units <- "miles"
+aerial <- TRUE
+elevation = TRUE
+xs_label_freq = 2
+exaggeration = 30
+extent_factor = 1.3
 output_dir    <- Sys.getenv("HOME")
 output_format <- "word_document"
 
 
-test_that("check level 2 report, no xs map", {
+test_that("check level 2 report, without xs map", {
   skip_if_no_arc()
   load_libraries()
 
   # Create report
-  fluvgeo::level_2_report(stream, flowline_fc, xs_fc, xs_points_fc,
-                          xs_dims_fc, dem, banklines_fc, features_fc,
-                          bf_estimate, regions, extent_factor, label_xs,
-                          show_xs_map,
-                          profile_units, output_dir, output_format)
-
+  p <- fluvgeo::level_2_report(stream, flowline_fc, xs_fc, xs_dims_fc,
+                               xs_points_1, xs_points_2,
+                               xs_points_3, xs_points_4,
+                               survey_name_1, survey_name_2,
+                               survey_name_3, survey_name_4,
+                               dem, banklines_fc, features_fc,
+                               bf_estimate, regions, label_xs,
+                               show_xs_map, profile_units,
+                               aerial, elevation,
+                               xs_label_freq, exaggeration, extent_factor,
+                               output_dir, output_format)
+  print(p)
   expect_true(file.exists(file.path(output_dir,
                                     "Sinsinawa_level_2_report.docx")))
 })
+
+# test_that("check level 2 report, with xs map", {
+#   skip_if_no_arc()
+#   load_libraries()
+#
+#   # Create report
+#   p <- fluvgeo::level_2_report(stream, flowline_fc, xs_fc, xs_dims_fc,
+#                                xs_points_1, xs_points_2,
+#                                xs_points_3, xs_points_4,
+#                                survey_name_1, survey_name_2,
+#                                survey_name_3, survey_name_4,
+#                                dem, banklines_fc, features_fc,
+#                                bf_estimate, regions, label_xs,
+#                                show_xs_map, profile_units,
+#                                aerial, elevation,
+#                                xs_label_freq, exaggeration, extent_factor,
+#                                output_dir, output_format)
+#   print(p)
+#   expect_true(file.exists(file.path(output_dir,
+#                                     "Sinsinawa_level_2_report.docx")))
+# })
