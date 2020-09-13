a2bbb0945f681b23b994f9f8f5843d0e9a568398
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-09-13 12:44:28 -0500

    added the cross_section_dimensions function

--- R/cross_section_dimensions_L1.R
+++ R/cross_section_dimensions_L1.R
@@ -4,7 +4,7 @@
 #' cross section feature class.
 #'
 #' @export
-#' @param xs                  SpatialLinesDataFrame; the full path to a
+#' @param xs_sf               sf object; the full path to a
 #'                            cross section line feature class
 #' @param lead_n              numeric; The number of features to lead/lag on
 #'                            either side of each feature that will be used to
@@ -14,25 +14,27 @@
 #'                            TRUE, FALSE (default)
 #' @param loess_span          numeric; the loess regression span parameter,
 #'                            defaults to 0.05
-#' @param vert_units	        character; The vertical units. One of: "m"
+#' @param vert_units	        character; The DEM vertical units. One of: "m"
 #'                            (meter), "ft" (foot), "us-ft" (us survey foot)
 #'
-#' @return A new cross section data frame with the hydraulic geometry
-#'      dimensions added.
+#' @return A new cross section data frame with the Level 1 hydraulic geometry
+#' dimensions added.
 #'
+#' @importFrom sf as_Spatial
 #'
-#' @importFrom dplyr bind_rows select
-#'
-cross_section_dimensions_L1 <- function(xs, lead_n,
+cross_section_dimensions_L1 <- function(xs_sf, lead_n,
                                      use_smoothing, loess_span, vert_units) {
   # Check inputs
-  check_cross_section(xs, "station_points")
+  check_cross_section(xs_sf, "station_points")
 
   # Subset xs for the current reach
-  xs_reach <- xs[xs@data$ReachName == unique(xs@data$ReachName), ]
+  xs_reach <- xs_sf[xs_sf$ReachName == unique(xs_sf$ReachName), ]
+
+  # Convert to sp for slope_sinuosity
+  xs_reach_sp <- sf::as_Spatial(xs_reach)
 
   # Calculate slope and sinuosity for xs_reach
-  xs_reach_ss <- fluvgeo::slope_sinuosity(xs_reach,
+  xs_reach_ss <- fluvgeo::slope_sinuosity(channel_features = xs_reach_sp,
                                           lead_n = lead_n, lag_n = 0,
                                           use_smoothing = use_smoothing,
                                           loess_span = loess_span,
--- tests/testthat/test_cross_section_dimensions_L1.R
+++ tests/testthat/test_cross_section_dimensions_L1.R
@@ -12,10 +12,7 @@ xs_dims_fc    <- file.path(system.file("extdata", "testing_Cole_2016.gdb",
 xs_dims_sf <- fluvgeo::fc2sf(xs_dims_fc)
 
 # Select fields available at level 1
-xs_dims_L1_sf <- dplyr::select(xs_dims_sf, 1:15)
-
-# Convert to sf
-xs <- sf::as_Spatial(xs_dims_L1_sf)
+xs_sf <- dplyr::select(xs_dims_sf, 1:15)
 
 # Set parameters
 lead_n = 1
@@ -24,10 +21,13 @@ loess_span = 0.5
 vert_units = "ft"
 
 # Calculate cross section dimensions
-xs_dims <- cross_section_dimensions_L1(xs = xs,
+xs_dims <- cross_section_dimensions_L1(xs_sf = xs_sf,
                                        lead_n = lead_n,
                                        use_smoothing = use_smoothing,
                                        loess_span = loess_span,
                                        vert_units = vert_units)
 
-
+test_that("check cross section dimensions L1", {
+  expect_true(check_cross_section_dimensions(xs_dims, "level_1"))
+  expect_error(check_cross_section_dimensions(xs_dims, "shear_stress"))
+})
