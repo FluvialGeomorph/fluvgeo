31e509793d675ff6658331e72ae57df44f2f106f
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-09-17 12:35:41 -0500

    added the esri_raster2RasterLayer function

---
+++ R/esri_raster2RasterLayer.R
@@ -0,0 +1,38 @@
+#' @title Convert an ESRI raster to a RasterLayer
+#'
+#' @description Converts an ESRI raster to a raster::RasterLayer.
+#'
+#' @usage Rasters stored in ESRI Geodatabases can currently only be read using
+#' the `arcgisbinding`
+#'
+#' @export
+#' @param raster_path       character; A path to an ESRI raster.
+#'
+#' @return A `raster::RasterLayer` object.
+#'
+#' @importFrom arcgisbinding arc.open as.raster arc.raster
+#' @importFrom raster crs
+#' @importFrom rgdal showSRID
+#' @importFrom sp CRS
+#'
+esri_raster2RasterLayer <- function(raster_path) {
+  # Open the path the the esri raster
+  raster_arc <- arcgisbinding::arc.open(raster_path)
+
+  # Convert the esri raster's CRS to valis WKT2
+  raster_wkt2 <- rgdal::showSRID(raster_arc@sr$WKT)
+
+  # Check spatial reference system WKT2 string
+  testthat::expect_true(rgdal::checkCRSArgs_ng(raster_wkt2)[[1]])
+
+  # Create CRS
+  raster_CRS <- sp::CRS(SRS_string = raster_wkt2)
+
+  # Convert to RasterLayer
+  raster_layer <- arcgisbinding::as.raster(arcgisbinding::arc.raster(raster_arc))
+
+  # Assign the correct CRS to the raster
+  raster::crs(raster_layer) <- raster_CRS
+
+  return(raster_layer)
+}
---
+++ tests/testthat/test_esri_raster2RasterLayer.R
@@ -0,0 +1,50 @@
+library(fluvgeo)
+library(raster)
+library(rgdal)
+library(sp)
+context("esri_raster2RasterLayer")
+
+# The `map_xs` function needs `arcgisbinding` to read a file geodatabase raster.
+# No other means currently exist to read file geodatabase rasters into R.
+
+# Helper functions
+skip_if_no_arc <- function() {
+  testthat::skip_if_not_installed("arcgisbinding")
+}
+
+load_libraries <- function() {
+  library(fluvgeo)
+  library(raster)
+  library(rgdal)
+  library(sp)
+  library(arcgisbinding)
+  arcgisbinding::arc.check_product()
+}
+
+raster_path <- file.path(system.file("extdata", "testing_raster.gdb",
+                                     package = "fluvgeo"),
+                         "dem_1m")
+tif_path    <- file.path(system.file("extdata", "dem_1m.tif",
+                                     package = "fluvgeo"))
+
+test_that("convert ESRI GDB raster to RasterLayer", {
+  skip_if_no_arc()
+  load_libraries()
+
+  # Create map
+  esri_raster <- esri_raster2RasterLayer(raster_path = raster_path)
+  plot(esri_raster)
+
+  expect_true("RasterLayer" %in% class(esri_raster))
+})
+
+test_that("convert .tif raster to RasterLayer", {
+  skip_if_no_arc()
+  load_libraries()
+
+  # Create map
+  tif_raster <- esri_raster2RasterLayer(raster_path = tif_path)
+  plot(tif_raster)
+
+  expect_true("RasterLayer" %in% class(tif_raster))
+})
