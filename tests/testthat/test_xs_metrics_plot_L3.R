433c3105cba53a6df6ed46c21bddab71b574d6b6
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-09-20 17:57:06 -0500

    fixed xs_metric_plot_L3 to use new L3 data

--- tests/testthat/test_xs_metrics_plot_L3.R
+++ tests/testthat/test_xs_metrics_plot_L3.R
@@ -1,5 +1,5 @@
 library(fluvgeo)
-context("xs_metrics_plot")
+context("xs_metrics_plot_L3")
 
 # Get feature class test data
 features_fc   <- file.path(system.file("extdata", "testing_Cole_2016.gdb",
@@ -7,7 +7,7 @@ features_fc   <- file.path(system.file("extdata", "testing_Cole_2016.gdb",
                            "features")
 xs_dims_fc    <- file.path(system.file("extdata", "testing_Cole_2016.gdb",
                                        package = "fluvgeo"),
-                           "xs_250_25_dims_L2")
+                           "xs_250_25_dims_L3")
 
 # Convert feature classes to an sf objects
 xs_dims_sf   <- fluvgeo::fc2sf(xs_dims_fc)
@@ -18,14 +18,14 @@ xs_label_freq = 2
 profile_units = "miles"
 
 # Call the xs_metrics_plot function
-level_3_metrics <- xs_metrics_plot_L2(xs_dims_sf = xs_dims_sf,
+level_3_metrics <- xs_metrics_plot_L3(xs_dims_sf = xs_dims_sf,
                                       features_sf = features_sf,
                                       label_xs = label_xs,
                                       xs_label_freq = xs_label_freq,
                                       profile_units = profile_units)
 print(level_3_metrics)
 
-test_that("check xs metrics plot", {
+test_that("check xs_metrics_plot_L3", {
   expect_true("ggplot" %in% class(level_3_metrics))
   expect_error(print(level_3_metrics), NA)
 })
