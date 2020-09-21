149a368593c49df45a51b6ca63109cf28d14d3dd
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-09-20 19:16:42 -0500

    added coverage of L2 and L3 for map_reach_metric

--- tests/testthat/test_map_reach_metric.R
+++ tests/testthat/test_map_reach_metric.R
@@ -29,30 +29,48 @@ wdr_map_sp <- map_reach_metric(wdr,
 print(wdr_map_sp)
 
 # sf
-# Get feature class test data in sf format
-flowline_fc <- file.path(system.file("extdata", "testing_data.gdb",
-                                        package = "fluvgeo"),
-                            "flowline")
-xs_dimensions_fc <- file.path(system.file("extdata", "testing_data.gdb",
-                                     package = "fluvgeo"),
-                             "riffle_floodplain_dims_planform")
 
-# Convert feature classes to an sf objects
-flowline_sf      <- fluvgeo::fc2sf(flowline_fc)
-xs_dimensions_sf <- fluvgeo::fc2sf(xs_dimensions_fc)
+# Get feature class test data in sf format
+flowline_fc     <- file.path(system.file("extdata", "testing_Cole_2016.gdb",
+                                         package = "fluvgeo"),
+                             "flowline")
+# Level 2
+xs_dimensions_L2_fc <- file.path(system.file("extdata", "testing_Cole_2016.gdb",
+                                             package = "fluvgeo"),
+                                 "xs_250_25_dims_L2")
+# Level 3
+xs_dimensions_L3_fc <- file.path(system.file("extdata", "testing_Cole_2016.gdb",
+                                             package = "fluvgeo"),
+                                 "xs_250_25_dims_L3")
+
+# Convert feature classes to an sf objects, Level 2
+flowline_sf         <- fluvgeo::fc2sf(flowline_fc)
+xs_dimensions_L2_sf <- fluvgeo::fc2sf(xs_dimensions_L2_fc)
+xs_dimensions_L3_sf <- fluvgeo::fc2sf(xs_dimensions_L3_fc)
 
 # Create the reach metric map using sf input
-wdr_map_sf <- map_reach_metric(wdr,
-                               flowline = flowline_sf,
-                               xs_dimensions = xs_dimensions_sf)
-print(wdr_map_sf)
+wdr_map_L2_sf <- map_reach_metric(wdr,
+                                  flowline = flowline_sf,
+                                  xs_dimensions = xs_dimensions_L2_sf)
+print(wdr_map_L2_sf)
+
+# Create the reach metric map using sf input, Level 3
+wdr_map_L3_sf <- map_reach_metric(wdr,
+                                  flowline = flowline_sf,
+                                  xs_dimensions = xs_dimensions_L3_sf)
+print(wdr_map_L3_sf)
 
 test_that("check map_reach_metric using sp input", {
   expect_true("tmap" %in% class(wdr_map_sp))
   expect_error(print(wdr_map_sp), NA)
 })
 
-test_that("check map_reach_metric using sf input", {
-  expect_true("tmap" %in% class(wdr_map_sf))
-  expect_error(print(wdr_map_sf), NA)
+test_that("check map_reach_metric using sf input, Level 2", {
+  expect_true("tmap" %in% class(wdr_map_L2_sf))
+  expect_error(print(wdr_map_L2_sf), NA)
+})
+
+test_that("check map_reach_metric using sf input, Level 3", {
+  expect_true("tmap" %in% class(wdr_map_L3_sf))
+  expect_error(print(wdr_map_L3_sf), NA)
 })
