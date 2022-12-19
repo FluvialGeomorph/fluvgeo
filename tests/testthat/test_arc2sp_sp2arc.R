2f6dcb7baff64478e81827353bf0f89793cd8070
Michael Dougherty <mike.p.doc.71@gmail.com>
2022-12-19 08:13:25 -0600

    added the get_arc_wkt function

---
+++ tests/testthat/test_arc2sp_sp2arc.R
@@ -0,0 +1,12 @@
+library(fluvgeo)
+context("arc2sp-sp2arc")
+
+load_libraries <- function() {
+  library(sp)
+  library(arcgisbinding)
+  arc.check_product()
+}
+
+fc_path <- file.path(system.file("extdata", "testing_data.gdb",
+                                 package = "fluvgeo"), "riffle_channel")
+
