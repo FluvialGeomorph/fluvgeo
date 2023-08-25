683283e189b3652bb185237c5e8811e09d543742
bchileen <barrie.chileen@gmail.com>
2023-08-25 15:47:39 -0500

    updated test_check_bankline_points for sf objects

--- tests/testthat/test_check_bankline_points.R
+++ tests/testthat/test_check_bankline_points.R
@@ -1,22 +1,22 @@
 library(fluvgeo)
 context("check_bankline_points")
 
-bankline_points <- fluvgeo::sin_bankline_points_sp
+bankline_points <- fluvgeo::sin_bankline_points_sf
 
 # Create some bad data
 ## Simulate the loop = 3, bend = 2, position = start point missing
 bad1 <- bankline_points
 #bad1_df <- bad1@data
-bad1@data[!is.na(bad1$loop) &
+bad1[!is.na(bad1$loop) &
           (bad1$loop == 3 & bad1$bend == 2 &
           !(bad1$position %in% c("apex", "end"))), ]$position <- NA
-bad1@data[!is.na(bad1$loop) &
+bad1[!is.na(bad1$loop) &
             (bad1$loop == 3 & bad1$bend == 2 &
                !(bad1$position %in% c("apex", "end"))), ]$bend <- NA
-bad1@data[!is.na(bad1$loop) &
+bad1[!is.na(bad1$loop) &
             (bad1$loop == 3 & bad1$bend == 2 &
              bad1$position %in% "apex"), ]$bend <- 0
-bad1@data[!is.na(bad1$loop) &
+bad1[!is.na(bad1$loop) &
             (bad1$loop == 3 & is.na(bad1$bend)), ]$loop <- NA
 #bad1_df <- bad1@data
 
@@ -30,7 +30,7 @@ test_that("check for malformed bankline_points", {
 })
 
 test_that("not bankline points", {
-  expect_error(check_bankline_points(fluvgeo::sin_flowline_sp))
-  expect_error(check_bankline_points(fluvgeo::sin_loop_points_sp))
-  expect_error(check_bankline_points(fluvgeo::sin_banklines_sp))
+  expect_error(check_bankline_points(fluvgeo::sin_flowline_sf))
+  expect_error(check_bankline_points(fluvgeo::sin_loop_points_sf))
+  expect_error(check_bankline_points(fluvgeo::sin_banklines_sf))
 })
