339c80dfc212c9bd5ff0693976db9ad15ad403d3
bchileen <barrie.chileen@gmail.com>
2023-08-31 08:56:41 -0500

    removed sp objects and replaced them with sf objects

--- R/xs2pts.R
+++ R/xs2pts.R
@@ -4,10 +4,10 @@
 #' to points.
 #'
 #' @export
-#' @param xs_sp          SpatialLinesDataFrame; a cross section data structure
+#' @param xs_sf          sf data frame; a cross section data structure
 #'                       created by the fluvgeo package.
 #'
-#' @return Returns a sp::SpatialPointsDataFrame of cross section lines converted
+#' @return Returns a sf::sf data frame of cross section lines converted
 #' to points.
 #'
 #' @examples
@@ -17,26 +17,16 @@
 #' # Call the xs2pts function for a cross section
 #' xs_pts <- xs2pts(xs_sf)
 #'
-#' @importFrom sp SpatialPointsDataFrame
+#' @importFrom sf st_drop_geometry st_as_sf st_geometry st_sf
 #'
 xs2pts <- function(xs_sf) {
-  coords      <- xs_sf[, c("POINT_X", "POINT_Y")]
-  data        <- xs_sf
-  crs <- sf::st_crs(xs_sf)
+  sf_df<-st_drop_geometry(xs_sf)
 
-  coords      <- xs_sp@data[, c("POINT_X", "POINT_Y")]
-  data        <- xs_sp@data
-  proj4string <- xs_sp@proj4string
 
-  xs_pts <- sp::SpatialPointsDataFrame(coords = coords,
-                                       data = data,
-                                       proj4string = proj4string)
-  xs_sf_sp<-st_as_sf(xs_pts)
+  xs_pts <- sf_df %>%
+    sf::st_as_sf(coords=c('POINT_X','POINT_Y')) %>%
+    sf::st_geometry()%>%
+    sf::st_sf(sf_df, crs=st_crs(xs_sf))
 
-  xs_pts2<- xs_sf %>%
-    select("POINT_X","POINT_Y")
 
-  xs_pts3<-sf::st_cast(xs_pts2, to="POINT", group_or_split=TRUE)
-
-  xs_pts2 <- sf::st_cast(xs_sf[,c("POINT_X", "POINT_Y")], to="POINT")
 }
