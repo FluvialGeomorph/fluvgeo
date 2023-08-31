41e67a2bef68c23ecd64b6ac2dd6619887df02a2
bchileen <barrie.chileen@gmail.com>
2023-08-31 08:55:35 -0500

    removed spatialdataframe and sp inputs and replaced them with sf outputs

--- R/xs2pts.R
+++ R/xs2pts.R
@@ -15,11 +15,15 @@
 #' xs_sp <- fluvgeo::sin_riffle_floodplain_dims_L3_sp
 #'
 #' # Call the xs2pts function for a cross section
-#' xs_pts <- xs2pts(xs_sp)
+#' xs_pts <- xs2pts(xs_sf)
 #'
 #' @importFrom sp SpatialPointsDataFrame
 #'
-xs2pts <- function(xs_sp) {
+xs2pts <- function(xs_sf) {
+  coords      <- xs_sf[, c("POINT_X", "POINT_Y")]
+  data        <- xs_sf
+  crs <- sf::st_crs(xs_sf)
+
   coords      <- xs_sp@data[, c("POINT_X", "POINT_Y")]
   data        <- xs_sp@data
   proj4string <- xs_sp@proj4string
@@ -27,4 +31,12 @@ xs2pts <- function(xs_sp) {
   xs_pts <- sp::SpatialPointsDataFrame(coords = coords,
                                        data = data,
                                        proj4string = proj4string)
+  xs_sf_sp<-st_as_sf(xs_pts)
+
+  xs_pts2<- xs_sf %>%
+    select("POINT_X","POINT_Y")
+
+  xs_pts3<-sf::st_cast(xs_pts2, to="POINT", group_or_split=TRUE)
+
+  xs_pts2 <- sf::st_cast(xs_sf[,c("POINT_X", "POINT_Y")], to="POINT")
 }
--- tests/testthat/test_xs2pts.R
+++ tests/testthat/test_xs2pts.R
@@ -2,13 +2,14 @@ library(fluvgeo)
 context("xs2pts")
 
 # Retrieve a cross section data structure
-xs_sp <- fluvgeo::sin_riffle_floodplain_dims_L3_sp
+xs_sf <- fluvgeo::sin_riffle_floodplain_dims_L3_sf
+
 
 # Call the xs2pts function for a cross section
-xs_pts <- xs2pts(xs_sp)
+xs_pts <- xs2pts(xs_sf)
 
 test_that("check xs_pts", {
-  expect_equal(class(xs_pts)[1], "SpatialPointsDataFrame")
-  expect_equal(xs_sp@proj4string, xs_pts@proj4string)
-  expect_equal(length(xs_sp$Seq), length(xs_pts$Seq))
+  expect_equal(class(xs_pts)[1], "sf")
+  expect_equal(sf::st_crs(xs_sf), sf::st_crs(xs_pts))
+  expect_equal(length(xs_sf$Seq), length(xs_pts$Seq))
 })
