181748fc4b4fc194de0603e67d8b54c8f4e3e5ca
Barrie Chileen Martinez <barrie.chileen@gmail.com>
2023-09-06 09:30:25 -0500

    replaced sp inputs with sf inputs

--- tests/testthat/test_check_valleyline.R
+++ tests/testthat/test_check_valleyline.R
@@ -2,11 +2,11 @@ library(fluvgeo)
 context("check_valleyline")
 
 test_that("check valleyline", {
-  expect_true(check_valleyline(fluvgeo::sin_valleyline_sp))
+  expect_true(check_valleyline(fluvgeo::sin_valleyline_sf))
 })
 
 test_that("check not valleyline", {
-  expect_error(check_valleyline(fluvgeo::sin_features_sp))
-  expect_error(check_valleyline(fluvgeo::sin_loop_points_sp))
-  expect_error(check_valleyline(fluvgeo::sin_bankline_points_sp))
+  expect_error(check_valleyline(fluvgeo::sin_features_sf))
+  expect_error(check_valleyline(fluvgeo::sin_loop_points_sf))
+  expect_error(check_valleyline(fluvgeo::sin_bankline_points_sf))
 })
