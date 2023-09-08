7f5b632335ab8061fdc7e4d25cc8a6e3c6bea9a3
Michael <mike.p.doc.71@gmail.com>
2023-09-08 14:07:16 -0500

    updated the fc2sf function to expose the quiet paramater

--- R/fc2sf.R
+++ R/fc2sf.R
@@ -7,13 +7,15 @@
 #'
 #' @param fc_path  character; Path to the feature class (e.g.,
 #'                 "C:/folder/geodatabase.gdb/feature_dataset/feature_class").
+#' @param quiet	   logical; suppress info on name, driver, size and spatial
+#'                 reference, or signaling no or multiple layers (sf::st_read)
 #'
 #' @return An `sf` object.
 #'
 #' @importFrom assertthat assert_that
 #' @importFrom sf st_read
 #'
-fc2sf <- function(fc_path) {
+fc2sf <- function(fc_path, quiet = FALSE) {
   feature_dataset <- dirname(fc_path)
   gdb             <- dirname(feature_dataset)
   assert_that(tools::file_ext(feature_dataset) != "gdb" &
@@ -22,5 +24,7 @@ fc2sf <- function(fc_path) {
 
   fc <- basename(fc_path)
 
-  fc_sf <- sf::st_read(gdb, fc, stringsAsFactors = FALSE)
+  fc_sf <- sf::st_read(gdb, fc,
+                       quiet = quiet,
+                       stringsAsFactors = FALSE)
 }
