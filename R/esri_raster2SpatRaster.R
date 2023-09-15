f65b4adbd85b774ee0ab06aedf00cda76b3f620d
Michael <mike.p.doc.71@gmail.com>
2023-09-15 13:02:44 -0500

    added tests for esri_raster2SpatRaster

--- R/esri_raster2SpatRaster.R
+++ R/esri_raster2SpatRaster.R
@@ -5,16 +5,23 @@
 #' @export
 #' @param raster_path       character; A path to an ESRI raster.
 #'
-#' @details Rasters stored in ESRI Geodatabases can currently only be read using
-#' the `arcgisbinding`
+#' @details Rasters stored in ESRI Geodatabases have historically only been
+#' accessible using the `arcgisbinding` package. No open source drivers existed
+#' to read file geodatabase rasters into R. However, since the release of GDAL
+#' v3.7, the OpenFileGDB driver now provides support for GDB rasters. GDAL
+#' access in R is currently best provided using `sf`. Check
+#' `sf::sf_extSoftVersion()` for the version of GDAL bundled with `sf`. When
+#' `sf` migrates to using this GDAL version and the OpenFileGDB raster driver
+#' is determined to be working, this function will migrate to using it instead
+#' of the proprietary `arcgisbinding` approach used up to now.
 #'
 #' @return A `terra::SpatRaster` object.
 #'
 #' @importFrom arcgisbinding arc.open as.raster arc.raster
-#' @importFrom terra crs
+#' @importFrom terra crs rast
 #' @importFrom sf st_crs
 #'
-esri_raster2RasterLayer <- function(raster_path) {
+esri_raster2SpatRaster <- function(raster_path) {
   # Open the path the the esri raster
   raster_arc <- arcgisbinding::arc.open(raster_path)
 
---
+++ tests/testthat/test_esri_raster2SpatRaster.R
@@ -0,0 +1,42 @@
+# The `esri_raster2SpatRaster` function needs `arcgisbinding` to read a file
+# geodatabase raster.
+
+# Helper functions
+skip_if_no_arc <- function() {
+  testthat::skip_if_not_installed("arcgisbinding")
+}
+
+load_libraries <- function() {
+  library(arcgisbinding)
+  arcgisbinding::arc.check_product()
+}
+
+raster_path <- file.path(system.file("extdata", "testing_raster.gdb",
+                                     package = "fluvgeo"),
+                         "dem_1m")
+tif_path    <- file.path(system.file("extdata", "dem_1m.tif",
+                                     package = "fluvgeo"))
+
+test_that("convert ESRI GDB raster to SpatRaster", {
+  skip_if_no_arc()
+  load_libraries()
+
+  gdb_raster <- esri_raster2SpatRaster(raster_path = raster_path)
+  #plot(gdb_raster)
+
+  expect_true("SpatRaster" %in% class(gdb_raster))
+  expect_equal(dim(gdb_raster), c(1887, 1142, 1))
+  expect_true(terra::same.crs(gdb_raster, terra::rast(tif_path)))
+})
+
+test_that("convert .tif raster to SpatRaster", {
+  skip_if_no_arc()
+  load_libraries()
+
+  tif_raster <- esri_raster2SpatRaster(raster_path = tif_path)
+  #plot(tif_raster)
+
+  expect_true("SpatRaster" %in% class(tif_raster))
+  expect_equal(dim(tif_raster), c(1887, 1142, 1))
+  expect_true(terra::same.crs(tif_raster, terra::rast(tif_path)))
+})
