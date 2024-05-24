2658d04de8d181411a09b8c896ca2a848f5ddba3
Michael <mike.p.doc.71@gmail.com>
2024-05-24 14:00:50 -0500

    started rosgen_classification function

---
+++ tests/testthat/test_rosgen_classification.R
@@ -0,0 +1,36 @@
+library(fluvgeo)
+library(dplyr)
+library(sf)
+
+test_that("check cross section dimensions L2 from GDB input", {
+  # Get feature class test data
+  xs_dims_fc    <- file.path(system.file("extdata", "y2016_R1.gdb",
+                                         package = "fluvgeo"),
+                             "feature_dataset/xs_50")
+  xs_points_fc  <- file.path(system.file("extdata", "y2016_R1.gdb",
+                                         package = "fluvgeo"),
+                             "feature_dataset/xs_50_points")
+
+  # Convert to sf
+  xs  <- fluvgeo::fc2sf(xs_dims_fc)
+  xs_points <- fluvgeo::fc2sf(xs_points_fc)
+
+  # Set parameters
+  bankfull_elevation <- 104
+  lead_n <- 3
+  use_smoothing <- TRUE
+  loess_span <- 0.5
+  vert_units <- "ft"
+
+  # Calculate cross section dimensions
+  xs_dims_1 <- cross_section_dimensions_L2(xs = xs,
+                                           xs_points = xs_points,
+                                           bankfull_elevation = bankfull_elevation,
+                                           lead_n = lead_n,
+                                           use_smoothing = use_smoothing,
+                                           loess_span = loess_span,
+                                           vert_units = vert_units)
+
+  expect_true(check_cross_section_dimensions(xs_dims_1,
+                                             "rosgen_class"))
+})
