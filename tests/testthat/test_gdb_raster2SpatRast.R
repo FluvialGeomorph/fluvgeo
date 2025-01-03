32294c1e66b31e1805534c9aa9bd97729dbb4aac
Michael <mike.p.doc.71@gmail.com>
2025-01-03 16:58:27 -0600

    added new function gdb_raster2SpatRast

---
+++ tests/testthat/test_gdb_raster2SpatRast.R
@@ -0,0 +1,26 @@
+
+test_that("convert geodatabase raster to SpatRast", {
+  raster_path <- file.path(system.file("extdata", "testing_raster.gdb",
+                                     package = "fluvgeo"),
+                         "dem_1m")
+
+  raster <- gdb_raster2SpatRast(raster_path)
+  expect_true("SpatRaster" %in% class(raster))
+  expect_true("dem_1m" %in% names(raster))
+  expect_equal(1887, terra::nrow(raster))
+  expect_equal(1142, terra::ncol(raster))
+  expect_equal(1,    terra::nlyr(raster))
+})
+
+test_that("convert file raster to SpatRast", {
+  raster_path <- file.path(system.file("extdata", "dem_1m.tif",
+                                       package = "fluvgeo"))
+
+  raster <- gdb_raster2SpatRast(raster_path)
+  expect_true("SpatRaster" %in% class(raster))
+  expect_true("Band_1" %in% names(raster))
+  expect_equal(1887, terra::nrow(raster))
+  expect_equal(1142, terra::ncol(raster))
+  expect_equal(1,    terra::nlyr(raster))
+})
+
