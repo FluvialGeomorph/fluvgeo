b47c5ca4ce90a03fc448d40f890128c884ec8e71
Michael <mike.p.doc.71@gmail.com>
2025-04-12 16:01:06 -0500

    export function arcgis_auth

--- R/arcgis_auth.R
+++ R/arcgis_auth.R
@@ -1,9 +1,10 @@
 #' Authenticate to an ArcGIS Enterprise Portal
 #'
 #' @description Authenticates to an ArcGIS Enterprise Portal.
-#' 
+#'
 #' @return an httr token
-#' 
+#' @export
+#'
 #' @importFrom arcgisutils auth_client set_arc_token arc_token
 #'
 arcgis_auth <- function() {
@@ -13,9 +14,9 @@ arcgis_auth <- function() {
     secret = Sys.getenv("ARCGIS_CLIENTSECRET"),
     host = Sys.getenv("ARCGIS_HOST"),
     expiration = 60)
-  
+
   # set the arc_token for the current session
   set_arc_token(token = client_token)
-  
+
   return(arc_token())
 }
