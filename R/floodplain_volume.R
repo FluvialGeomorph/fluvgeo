206a2119363d2cad98b1f1ec7ef88cf8dc70e759
Michael <mike.p.doc.71@gmail.com>
2025-04-19 12:11:23 -0500

    updated floodplain_volume to more accurately calculate cell area and improved tests

--- R/floodplain_volume.R
+++ R/floodplain_volume.R
@@ -9,7 +9,7 @@
 #' in cubic meters.
 #' @export
 #'
-#' @importFrom terra not.na ifel linearUnits
+#' @importFrom terra not.na ifel cellSize values
 #'
 floodplain_volume <- function(dem, watersurface) {
   assert_that("SpatRaster" %in% class(dem),
@@ -17,7 +17,7 @@ floodplain_volume <- function(dem, watersurface) {
   assert_that("SpatRaster" %in% class(watersurface),
               msg = "watersurface must be a SpatRaster object")
 
-  # Set dem NA to match watersurface
+  # Set dem NAs to match watersurface raster NAs (make data areas match)
   dem_na <- ifel(not.na(watersurface), dem, NA)
 
   # Calculate difference between watersurface raster and dem raster
@@ -29,9 +29,12 @@ floodplain_volume <- function(dem, watersurface) {
   # Convert depth units from feet to meters
   depth_m <- depth_ft * 0.3048
 
-  # Calculate cell volume
-  cell_length_m <- linearUnits(dem)
-  cell_m3 <- cell_length_m ^ 3
+  # Calculate average cell area
+  cell_area_m <- mean(values(cellSize(dem, unit = "m")))
+
+  # Calculate cell volume (area * depth at 1 m)
+  cell_m3 <- cell_area_m * 1
+
   # cell depth * cell volume
   vol_m3 <- sum((values(depth_m) * cell_m3), na.rm = TRUE)
 
--- tests/testthat/test_floodplain_volume.R
+++ tests/testthat/test_floodplain_volume.R
@@ -1,9 +1,9 @@
-channel_plot <- function(flowline, rem, bankfull) {
-  terra::plot(rem < bankfull)
+channel_plot <- function(flowline, rem, water_surface_elevation) {
+  terra::plot(rem < water_surface_elevation)
   terra::lines(flowline, col = "blue")
 }
 
-test_that("check detrend", {
+test_that("check channel volume", {
   fl_mapedit <- sf::st_read(system.file("extdata", "shiny", "fl_mapedit.shp",
                                         package = "fluvgeodata"), quiet = TRUE)
   fl_fix <- sf_fix_crs(fl_mapedit)
@@ -18,7 +18,54 @@ test_that("check detrend", {
   rem <- detrend$rem
   trend <- detrend$trend
   watersurface <- trend + 2                     # water depth = baseline + 2 ft
-  #channel_plot(flowline, rem, 102)
+  channel_plot(flowline, rem, 102)
   fpv <- floodplain_volume(dem, watersurface)
   expect_true(is.numeric(fpv))
+})
+
+test_that("check floodplain volume", {
+  fl_mapedit <- sf::st_read(system.file("extdata", "shiny", "fl_mapedit.shp",
+                                        package = "fluvgeodata"), quiet = TRUE)
+  fl_fix <- sf_fix_crs(fl_mapedit)
+  fl_3857 <- sf::st_transform(fl_fix, crs = 3857) # Web Mercator
+  reach_name <- "current stream"
+  dem <- get_dem(fl_3857)
+  flowline <- flowline(fl_3857, reach_name, dem)
+  station_distance = 5
+  flowline_points <- flowline_points(flowline, dem, station_distance)
+  buffer_distance <- 300
+  detrend <- detrend(dem, flowline, flowline_points, buffer_distance)
+  rem <- detrend$rem
+  trend <- detrend$trend
+  watersurface <- trend + 18                   # water depth = baseline + 18 ft
+  channel_plot(flowline, rem, 118)
+  fpv <- floodplain_volume(dem, watersurface)
+  expect_true(is.numeric(fpv))
+})
+
+test_that("check volume difference between channel and floodplain surfaces", {
+  fl_mapedit <- sf::st_read(system.file("extdata", "shiny", "fl_mapedit.shp",
+                                        package = "fluvgeodata"), quiet = TRUE)
+  fl_fix <- sf_fix_crs(fl_mapedit)
+  fl_3857 <- sf::st_transform(fl_fix, crs = 3857) # Web Mercator
+  reach_name <- "current stream"
+  dem <- get_dem(fl_3857)
+  flowline <- flowline(fl_3857, reach_name, dem)
+  station_distance = 5
+  flowline_points <- flowline_points(flowline, dem, station_distance)
+  buffer_distance <- 300
+  detrend <- detrend(dem, flowline, flowline_points, buffer_distance)
+  rem <- detrend$rem
+  trend <- detrend$trend
+  channel_watersurface <- trend + 2             # water depth = baseline +2 ft
+  floodplain_watersurface <- trend + 18         # water depth = baseline +18 ft
+  channel_surface    <- ifel(channel_watersurface > dem,
+                             channel_watersurface, dem)
+  floodplain_surface <- ifel(floodplain_watersurface > dem,
+                             floodplain_watersurface, dem)
+  channel_plot(flowline, rem, 118)
+  cv  <- floodplain_volume(dem, channel_surface)
+  fpv <- floodplain_volume(dem, floodplain_surface)
+  dif <- floodplain_volume(channel_surface, floodplain_surface)
+  expect_true(cv < fpv)
 })
