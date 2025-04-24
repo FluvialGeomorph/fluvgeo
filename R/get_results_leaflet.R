cca4775578632dbf44187bb22bdb99b721364e86
Michael <mike.p.doc.71@gmail.com>
2025-04-24 12:37:12 -0500

    add channel_ and floodplain_polys to get_results_leaflet

--- R/get_results_leaflet.R
+++ R/get_results_leaflet.R
@@ -1,37 +1,60 @@
 #' @title Get Results Leaflet
 #'
-#' @param fl    sf object;
-#' @param xs    sf object;
-#' @param dem   terra SpatRast object;
+#' @param fl               sf object; A flowline object.
+#' @param xs               sf object; A cross section object.
+#' @param dem              terra SpatRast object; A DEM raster.
+#' @param channel_poly     sf object; A channel polygon.
+#' @param floodplain_poly  sf object; A floodplain poly object.
 #'
 #' @returns a leaflet object
 #' @export
 #'
 #' @importFrom assertthat assert_that
-#' @importFrom leaflet addPolylines
+#' @importFrom leaflet addPolylines addPolygons
 #' @importFrom leafem updateLayersControl
 #' @importFrom sf st_transform
-#' 
-get_results_leaflet <- function(fl, xs, dem) {
-  assert_that("sf" %in% class(fl), 
+#'
+get_results_leaflet <- function(fl, xs, dem,
+                                channel_poly = NULL, floodplain_poly = NULL) {
+  assert_that("sf" %in% class(fl),
               msg = "fl must be an sf object")
-  assert_that("sf" %in% class(xs), 
+  assert_that("sf" %in% class(xs),
               msg = "xs must be an sf object")
-  assert_that("SpatRaster" %in% class(dem), 
+  assert_that("SpatRaster" %in% class(dem),
               msg = "dem must be a SpatRaster object")
+  assert_that("sf" %in% class(channel_poly),
+              msg = "channel_poly must be an sf object")
+  assert_that("sf" %in% class(floodplain_poly),
+              msg = "floodplain_poly must be an sf object")
   assert_that(check_crs_3857(fl), msg = "fl must be crs 3857")
   assert_that(check_crs_3857(xs), msg = "xs must be crs 3857")
   assert_that(check_crs_3857(dem), msg = "dem must be crs 3857")
-  
-  results_leaflet <- 
+  assert_that(check_crs_3857(channel_poly),
+              msg = "channel_poly must be crs 3857")
+  assert_that(check_crs_3857(floodplain_poly),
+              msg = "floodplain_poly must be crs 3857")
+
+  results_leaflet <-
     get_terrain_leaflet(xs, dem) %>%
     addPolylines(
       data = st_transform(fl, crs = 4326),
-      color = "blue",
+      layerId = "flowline",
+      color = "blue", weight = 1,
       group = "Flowline") %>%
+    addPolygons(
+      data = st_transform(channel_poly, crs = 4326),
+      layerId = "channel_poly",
+      color = "navy", weight = 1,
+      group = "Channel") %>%
+    addPolygons(
+      data = st_transform(floodplain_poly, crs = 4326),
+      layerId = "floodplain_poly",
+      color = "forestgreen", weight = 1,
+      group = "Floodplain") %>%
     updateLayersControl(
-      addOverlayGroups = c("Elevation", "Cross Sections", "Flowline"),
+      addOverlayGroups = c("Elevation", "Cross Sections", "Flowline",
+                           "Channel", "Floodplain"),
       position = "topleft")
-  
+
   return(results_leaflet)
 }
--- tests/testthat/test_get_results_leaflet.R
+++ tests/testthat/test_get_results_leaflet.R
@@ -1,17 +1,31 @@
 # R 4.4.0 has bug that cannot display js-based htmlwidgets in viewer
 
-test_that("leaflet from geojson", {
-  fl_shp <- sf::st_read(system.file("extdata", "shiny", "fl.shp",
-                                    package = "fluvgeodata"), quiet = TRUE)
-  fl_fix <- sf_fix_crs(fl_shp)
-  fl <- sf::st_transform(fl_fix, crs = 3857) # Web Mercator
-  xs_shp <- sf::st_read(system.file("extdata", "shiny", "xs.shp",
-                                    package = "fluvgeodata"), quiet = TRUE)
-  xs_fix <- sf_fix_crs(xs_shp)
+test_that("channel and floodplain polys", {
+  xs_mapedit <- sf::st_read(system.file("extdata", "shiny", "xs_mapedit.shp",
+                                        package = "fluvgeodata"), quiet = TRUE)
+  xs_fix <- sf_fix_crs(xs_mapedit)
   xs <- sf::st_transform(xs_fix, crs = 3857) # Web Mercator
-  xs$Seq <- row.names(xs)
+  fl_mapedit <- sf::st_read(system.file("extdata", "shiny", "fl_mapedit.shp",
+                                        package = "fluvgeodata"), quiet = TRUE)
+  fl_fix <- sf_fix_crs(fl_mapedit)
+  fl_3857 <- sf::st_transform(fl_fix, crs = 3857) # Web Mercator
+  reach_name <- "current stream"
   dem <- get_dem(xs)
-  leaf <- get_results_leaflet(fl, xs, dem)
+  fl <- flowline(fl_3857, reach_name, dem)
+  station_distance = 5
+  flowline_points <- flowline_points(fl, dem, station_distance)
+  buffer_distance <- 300
+  detrend <- detrend(dem, fl, flowline_points, buffer_distance)
+  rem <- detrend$rem
+  trend <- detrend$trend
+  xs <- cross_section(xs, flowline_points)
+  station_distance = 5
+  xs_pts <- cross_section_points(xs, dem, rem, station_distance)
+  channel_wse <- 103
+  channel_poly <- water_surface_poly(rem, channel_wse, fl)
+  floodplain_wse <- 112
+  floodplain_poly <- water_surface_poly(rem, floodplain_wse, fl)
+  leaf <- get_results_leaflet(fl, xs, dem, channel_poly, floodplain_poly)
   leaf
   expect_true("leaflet" %in% class(leaf))
 })
